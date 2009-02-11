5852f09909e5ed323cb6658ba0c2c5b76f662333
David.Jurgens <David.Jurgens@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-02-11 05:40:12 +0000

    A    hand-coded-analogies.txt
    - Added a list of lists of analogous pairs.  This is intended to be pickled and
      used by the SAT-question solver.  Also, this is just for reference to verify
      that the approach works.  Far too many relationship types are missing.
    
    A    testLists2.pickle
    
    - Added a pickled version of the hand-coded-analogies.txt
    
    M    SATSolver.py
    
    - Added check for whether our guess was right
    
    - Added final count of how many analogies we got right
    
    - Fixed bug where leaf nodes would divide by zero when trying to determine their
      average children's depth
    
    - Fixed bugs with erroneous names
    
    - Added more precomputed avg.-child-leaf-depth values for the largest synsets to
      speed things up
    
    - Improved output and removed most debugging output
    
    A    ListPickler.py
    
    - Added the first version of a script that will pickle a flat file of blank like
      separated lists of analogous pairs (see hand-coded-analogies.txt for an
      example)

--- trunk/SATSolver.py
+++ trunk/SATSolver.py
@@ -62,12 +62,16 @@ def loadQuestions(SATfile):
 def answerSATQuestion(SATQuestion, listOfListOfAnalogousPairs):
 
     source = SATQuestion[0];
-    print "searching for answer to %s:%s" % (source)
+    print "Question %s:%s" % (source)
     options = SATQuestion[1];
+    for option in options:
+        print "\t%s:%s" % (option)
     correctIndex = SATQuestion[2];
 
     listIndex = 0
     listOfScoreAndList = []
+    listToHighestSimilarity = dict()
+
     # find the list of tuples which maximizes the similarity to the source pair
     for list in listOfListOfAnalogousPairs:
         highestSimilarity = 0
@@ -97,15 +101,24 @@ def answerSATQuestion(SATQuestion, listOfListOfAnalogousPairs):
                 if (similarity > mostSimilar[0]):
                     mostSimilar = (similarity, (c, d))
 
-        print "selecting: %s:%s::%s:%s" % (source[0], source[1], 
+        print "SELECTING: %s:%s::%s:%s" % (source[0], source[1], 
                                            (mostSimilar[1])[0],
                                            (mostSimilar[1])[1])
-
         
+        if ((mostSimilar[1])[0] == (options[correctIndex])[0] and 
+            (mostSimilar[1])[1] == (options[correctIndex])[1]):
+            print "CORRECT!"
+            return True
+        else:
+            print "INCORRECT: %s:%s::%s:%s" % (source[0], source[1], 
+                                               (options[correctIndex])[0],
+                                               (options[correctIndex])[1])
+            return False
+
 def invComp(x, y):
-    if i < j:
+    if x < y:
         return 1
-    elif i == j:
+    elif x == y:
         return 0
     else:
         return -1
@@ -131,11 +144,26 @@ def getScaledSimilarityMeasure(synset1, synset2):
     similarity = getSimilarityMeasure(synset1, synset2)
     normDepth1 = getNormalizedDepth(synset1)
     normDepth2 = getNormalizedDepth(synset2)
-    return (2.0 * similiarity) / float(normDepth1 + normDepth2)
+    return (2.0 * similarity) / float(normDepth1 + normDepth2)
 
 # returns the normalized depth of the least common ancestor of the two synsets
 def getSimilarityMeasure(synset1, synset2):
-    ancestors = synset1.lowest_common_hypernyms(synset2)
+    #print "getSimMeas(%s, %s)" % (synset1, synset2)
+    ancestors = []
+    try:
+        ancestors = synset1.lowest_common_hypernyms(synset2)
+    except ValueError:
+        #print "ERROR: couldn't find common ancestor of %s and %s" % (synset1, synset2)
+        # PANIC: just add in all the root hypernyms of each synset
+        if True:
+            return 0
+        else:
+            ancestors = set()
+            for r in synset1.root_hypernyms():
+                ancestors.add(r)
+            for r in synset2.root_hypernyms():
+                ancestors.add(r)
+
     # NOTE: what do we do if there are more than one?  For now, choose the
     # deepest ancestor in hopes that it is the cloests to the two synsets
     deepest = (0, ())
@@ -144,6 +172,7 @@ def getSimilarityMeasure(synset1, synset2):
         if depth >= deepest[0]:
             deepest = (depth, ancestor)
     
+        
     return getNormalizedDepth(deepest[1])
 
 # Returns the depth of the synset divided by the average depth of its
@@ -154,9 +183,31 @@ def getNormalizedDepth(synset):
 AVG_DESCENDENT_DEPTH_CACHE = dict()
 # Returns the average depth of all leaf hyponyms under this synset
 def getAverageDescendantDepth(synset):
-    print "looking for avg. descendent depth of %s" % (synset)
-    if synset not in AVG_DESCENDENT_DEPTH_CACHE:
-        print "(was not cached)"
+    #print "looking for avg. descendent depth of %s" % (synset)
+    
+    # DEBUG: REMOVE ASAP
+    if synset == wn.synset('entity.n.01'):
+        return 9.396485
+    if synset == wn.synset('abstraction.n.06'):
+        return 8.695125
+    if synset == wn.synset('physical_entity.n.01'):
+        return 10.094137
+    if synset == wn.synset('social_group.n.01'):
+        return 8.550117
+    if synset == wn.synset('psychological_feature.n.01'):
+        return 9.650382
+    if synset == wn.synset('whole.n.02'):
+        return 10.663869
+    if synset == wn.synset('group.n.01'):
+        return 8.128212
+    if synset == wn.synset('object.n.01'):
+        return 10.490863
+    if synset == wn.synset('organism.n.01'):
+        return 11.179508
+    
+    if synset not in AVG_DESCENDENT_DEPTH_CACHE:    
+        #print "(%s was not cached)" % (synset)
+        depth = getDepth(synset)
         # find all of the leaf hyponyms under it and compute their depth
         hyponyms = []
         for s in synset.hyponyms():
@@ -164,7 +215,7 @@ def getAverageDescendantDepth(synset):
         alreadySeen = set()
         depthSum = 0
         leaves = 0
-        print "number of immediate hyponyms: %d" % (len(hyponyms))
+
         for hyponym in hyponyms:
             if hyponym in alreadySeen:
                 continue
@@ -184,10 +235,11 @@ def getAverageDescendantDepth(synset):
                 for hyp in nextLevel:
                     if hyp not in alreadySeen:
                         hyponyms.append(hyp)
-        depth = getDepth(synset)
-        print "depth %f, depthSum %f, leaves %f" % (depth, depthSum, leaves)
-        normDepth = depth / (float(depthSum) / float(leaves))
-        AVG_DESCENDENT_DEPTH_CACHE[synset] = normDepth
+        
+        avgDescDepth = float(depthSum) / float(leaves)if leaves > 0 else depth
+#        if leaves > 1000:
+#            print "%s: depth %f, leaves %d, avg desc. depth %f" % (synset, depth, leaves, avgDescDepth)
+        AVG_DESCENDENT_DEPTH_CACHE[synset] = avgDescDepth
         
     return AVG_DESCENDENT_DEPTH_CACHE[synset]
 
@@ -204,7 +256,7 @@ def getDepth(synset):
         avgDepth = float(pathLenSum) / len(hypernymPaths)
         DEPTH_CACHE[synset] = avgDepth
     depth = DEPTH_CACHE[synset]
-    print "depth(%s) = %f" % (synset, depth)
+    #print "depth(%s) = %f" % (synset, depth)
     return depth
 
 
@@ -224,8 +276,11 @@ def main(argv=None):
     listOfListOfAnalogousPairs = pickle.load(fileHandle)
 
     # compute the answers
+    correct = 0
     for question in listOfQuestions:
-        answerSATQuestion(question, listOfListOfAnalogousPairs)
+        if answerSATQuestion(question, listOfListOfAnalogousPairs):
+            correct += 1
+    print "Answered %d/%d correct" % (correct, len(listOfQuestions))
 
 
 def usage():
