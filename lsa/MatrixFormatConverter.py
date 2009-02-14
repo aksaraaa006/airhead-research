594684cbebe123b50c5455a4edcf58ebabedb8b3
David.Jurgens <David.Jurgens@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-02-14 03:28:10 +0000

    updated matric converter to use much less memory, but it now requires that the use manually prepend the matrix size to the start the file

--- lsa/MatrixFormatConverter.py
+++ lsa/MatrixFormatConverter.py
@@ -8,46 +8,61 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv
         
-    if len(argv) != 2:
+    if len(argv) != 3:
         usage();
         return None
+
+    output = open(argv[2], "w")
     
     # determine the dimensions and number of non-zero elements to the matrix
     nonZeroVals = 0
     cols = 0
     rows = 0
     input = open(argv[1])
-    colToRowVal = dict()
+
+    curCol = 0
+    rowsOfCurCol = []
 
     # lines are formated "col row val"
     for line in input:
         triple = line.split("\t")
         nonZeroVals += 1
-        row = int(triple[1].strip())
-        col = int(triple[0].strip())
+        # subtract 1 from row and column when moving from matlab 1-indexed
+        # matrices to SVDLIBC index 0-indexed matrices.
+        row = int(triple[1].strip()) - 1
+        col = int(triple[0].strip()) - 1
         val = float(triple[2].strip())
-        if col not in colToRowVal:
-            colToRowVal[col] = []
-        rowAndVals = colToRowVal[col]
-        rowAndVals.append((row, val))
+        
+        # print "col %d, row %d, val %d" % (col, row, val)
+
         if row > rows:
             rows = row
         if col > cols:
             cols = col
-    input.close()
 
-    print "%s %s %s" % (rows, cols, nonZeroVals)
-    
-    # print out the column indicies in order
-    for col in range(cols):
-        if col in colToRowVal:
-            rowAndVals = colToRowVal[col]
-            print "%d" % (len(rowAndVals))
-            for row, val in rowAndVals:
-                print "%s %s" % (row, val)
-        else:
-            print "0"
+        if col != curCol:     
+            
+            # output all the data for the current column
+            output.write("%d\n" % (len(rowsOfCurCol)))
 
+            for row, val in rowsOfCurCol:
+                output.write("%s %s\n" % (row, val))
+            
+            # ensure that we've covered all the row in between the now current
+            # row and the last one we saw in case we skippsed some rows
+            for skipped in range(curCol + 1, col):
+                output.write("0\n")
+
+            # last reset the current column and empty the list
+            curCol = col
+            rowsOfCurCol = []
+
+        rowsOfCurCol.append((row, val))
+            
+
+    output.close()
+    print "ADD THIS TO FILE AS HEADER:"
+    print "%s %s %s" % (rows + 1, cols + 1, nonZeroVals)    
 
 def usage():
     print "usage: <input term-doc-matrix> <SVDLIBC-formatted output file>"
