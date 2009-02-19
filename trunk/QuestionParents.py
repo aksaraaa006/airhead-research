dd492a467843e76f9313ec6467d9605898b1758f
FozzietheBeat <FozzietheBeat@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-02-19 07:44:15 +0000

    omg comments

--- trunk/QuestionParents.py
+++ trunk/QuestionParents.py
@@ -7,7 +7,10 @@ import SATSolver as ss
 import sys
 
 def getWordSimilarity(sat_word, possible_word):
-  # NB: I sure hope both of these words are nouns!
+  """sat_word is the noun from a SAT question, possible_word is expected to be a
+  synset name which we know to be a valid synset.  Check the similarity between
+  all the sense of sat_word and possible_word, returning the highest similarity
+  score found."""
   synsets1 = wordnet.synsets(sat_word, pos='n')
   if possible_word == "None":
     return 0
@@ -22,6 +25,11 @@ def getWordSimilarity(sat_word, possible_word):
   return highestSimilarity
 
 def readSatQuestions(in_file):
+  """Create a list of SAT questions, were each one is stored in a 3-tuple.  The
+  first element will be a tuple of noun pairs which are the questions, the
+  second element is a tuple of noun pairs which are the answer.  The third
+  element is a 5-tuple of 2-tuples, where each inner tuple is an option for the
+  question."""
   question_list = []
   sat_str = open(in_file).read()
   num_relations = 0
@@ -34,10 +42,15 @@ def readSatQuestions(in_file):
   return question_list
 
 def readParents(in_file):
+  """Produce a list of parent tuples extracted from the expansion lists.  Only
+  return parent tuples which do not contain empty parents."""
   expanded_tuples = pickle.load(open(in_file))
   return [parents for (exp, parents) in expanded_tuples if "None" not in parents]
 
 def isParent(parent, word):
+  """Check that the parent synset is in some hypernym path of word.  Any sense
+  of word which has parent in it's hypernym path is acceptable since we do not
+  do WSD on word."""
   if parent == "None":
     return False
   parent = wordnet.synset(parent)
@@ -49,13 +62,17 @@ def isParent(parent, word):
   return False
 
 def chooseOptions(options, l_parent, r_parent):
+  """Given a group of options, choose only those who generalize to left and
+  right parent."""
   return [opt for opt in options if isParent(l_parent, opt[0]) and
                                     isParent(r_parent, opt[1])]
 
 def attemptAnswer(question, answer, options, parent_list):
   """Return one of three values, 0, 1, or 2.  0 means the question was not
   answerable at all, 1 means it could be answered but with multiple options, and
-  2 means it could be answered perfectly"""
+  2 means it could be answered perfectly.  Additionally, a list of ambigious
+  options will be returned, where this list contains all the instances of
+  several options qualifying as an answer."""
   ambigious = 0
   multi_options = []
   for (l_parent, r_parent) in parent_list:
@@ -73,6 +90,11 @@ def attemptAnswer(question, answer, options, parent_list):
   return ambigious, multi_options
 
 def similarityAnswer(sat_file, expanded_file):
+  """Try to answer all the SAT questions by using a similarity metric.  For each
+  question, find all the expansion lists whose parents are hypernyms of the
+  question.  Then with each of these lists, compute which option has the most
+  similairity to each list, and choose the option which was most similar to the
+  most number of lists"""
   question_list = readSatQuestions(sat_file)
   expanded_tuples = pickle.load(open(expanded_file))
   exp_and_parents = [ (exp, paren) for (exp,paren) in expanded_tuples if
@@ -81,12 +103,20 @@ def similarityAnswer(sat_file, expanded_file):
   for (question, answer,options) in question_list:
     print "answering question: ", question
     good_expansions = []
+    
+    # Get the lists which generalize to the same parents as the question.
     for (expand_list, (l_parent, r_parent)) in exp_and_parents:
       if isParent(l_parent, question[0]) and isParent(r_parent, question[1]):
         good_expansions.append(expand_list)
+
+    # Zero out a score dictionary to keep track of which options have which
+    # score.
     option_scores = {}
     for opt in options:
       option_scores[opt] = 0
+
+    # Go through all the saved lists and check this list against each option.
+    # The option that is most similar to the current list gets a vote.
     for generalized_list in good_expansions:
       best_score = 0
       best_option = None
@@ -98,6 +128,8 @@ def similarityAnswer(sat_file, expanded_file):
           best_option = opt
       if best_option:
         option_scores[best_option] += 1
+
+    # Find the option which recieved the most votes.
     best_score = 0
     best_answer = None
     for key in option_scores:
@@ -106,6 +138,8 @@ def similarityAnswer(sat_file, expanded_file):
         best_answer = key
     print "best answer is: ", best_answer
     print "real answer is: ", answer
+
+    # We only really answered it correctly if the answer is right.
     if best_answer == answer:
       score += 1
   print score, len(question_list)
@@ -120,11 +154,20 @@ def getSimilarity(listOfAnalogousPairs, sourcePair):
     return highestSimilarity
 
 def answerSAT(sat_file, expanded_file):
+  """For all the SAT questions, attempt to answer it by finding some expansion
+  list parent which the question can generalize to.  Then, find all the options
+  which also generalize to the same parents, and if only the correct answer
+  generalized, we have answered the question correctly."""
   question_list = readSatQuestions(sat_file)
   parent_list = readParents(expanded_file)
-  ambig_dict = {}
+
+  # Keep track of a few metrics so we can digest the results.  Score is
+  # obviously how many we got right.  multiple is how many questions we could
+  # not answer definitively, but has some set of choices.  the ambig_dict will
+  # hold a list of the choices found for each question.
   score = 0
   multiple = 0
+  ambig_dict = {}
   for (question, answer,options) in question_list:
     answer, spares = attemptAnswer(question, answer, options, parent_list)
     if answer == 1:
