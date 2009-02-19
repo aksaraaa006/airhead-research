f8c5863142b1ddecf22583d38a684898de57e8bc
FozzietheBeat <FozzietheBeat@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-02-19 01:22:56 +0000

    rwak

--- trunk/QuestionParents.py
+++ trunk/QuestionParents.py
@@ -16,12 +16,12 @@ def readSatQuestions(in_file):
     print options
 
     second = question[ord(question[7]) - ord('a') + 2].split()
-    question_list.append(((first[0], first[1]), (second[0], second[1]), options))
+    question_list.append((first, second, options))
   return question_list
 
 def readParents(in_file):
   expanded_tuples = pickle.load(open(in_file))
-  return [parents for (exp, parents) in expanded_tuples]
+  return [parents for (exp, parents) in expanded_tuples if "None" not in parents]
 
 def isParent(parent, word):
   if parent == "None":
@@ -39,21 +39,35 @@ def chooseOptions(options, l_parent, r_parent):
                                     isParent(r_parent, opt[1])]
 
 def attemptAnswer(question, answer, options, parent_list):
+  """Return one of three values, 0, 1, or 2.  0 means the question was not
+  answerable at all, 1 means it could be answered but with multiple options, and
+  2 means it could be answered perfectly"""
+  ambigious = 0
   for (l_parent, r_parent) in parent_list:
-    chosen_options = chooseOptions(options, l_parent, r_parent)
-    if (isParent(l_parent, question[0]) and isParent(r_parent, question[1]) and
-        len(chosen_options) == 1 and chosen_options[0] == answer):
-      return True
-  return False
+    print "Checking question %s:%s with parents %s:%s" %(question[0],
+        question[1], l_parent, r_parent)
+    if isParent(l_parent, question[0]) and isParent(r_parent, question[1]):
+      chosen_options = chooseOptions(options, l_parent, r_parent)
+      if len(chosen_options) == 1 and chosen_options[0] == answer:
+        print "answer %s:%s chosen for question" %(answer[0], answer[1])
+        return 2 
+      elif len(chosen_options) > 0:
+        print "several options chosen for parents %s:%s" %(l_parent, r_parent)
+        ambigious = 1
+  return ambigious
 
 def answerSAT(sat_file, expanded_file):
   question_list = readSatQuestions(sat_file)
   parent_list = readParents(expanded_file)
   score = 0
+  multiple = 0
   for (question, answer,options) in question_list:
-    if attemptAnswer(question, answer, options, parent_list):
+    answer = attemptAnswer(question, answer, options, parent_list)
+    if answer == 1:
+      multiple += 1
+    elif answer == 2:
       score += 1
-  print score, len(question_list)
+  print score, multiple, len(question_list)
 
 if __name__ == "__main__":
   answerSAT(sys.argv[1], sys.argv[2])
