0713da75b207036dfd26b37e4d304c005e16b9fa
FozzietheBeat <FozzietheBeat@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-03-09 03:39:33 +0000

    fix the banner score

--- trunk/eval_pagerank/scorer.py
+++ trunk/eval_pagerank/scorer.py
@@ -37,62 +37,59 @@ def bannerSim(syn1, syn2, def_dict):
     score = bannerScore(syn1, syn2, def_dict[syn1], def_dict[syn2])
   return score
 
-def lengthMatch(def1, def2):
+def lengthMatch(def1, def2, i):
   iter1 = def1
-  iter2 = def2
   length = 0
-  while iter1 and iter2:
-    if iter1[0] == iter2[0]:
+  while iter1 and i < len(def2):
+    if iter1[0] == def2[i]:
       length += 1
+      def2[i] = None
     else:
       break
     iter1 = iter1[1:]
-    iter2 = iter2[1:]
+    i += 1
   return length * length
 
 def bannerScore(syn1, syn2, def1, def2):
   def1 = list(def1)
   def2 = list(def2)
   score = 0
-  sub_score = 0
   while def1:
-    iter_def2 = def2
-    while iter_def2:
-      if def1[0] == iter_def2[0]:
-        sub_score += lengthMatch(def1, iter_def2)
-      iter_def2 = iter_def2[1:]
+    for i in range(len(def2)):
+      if def1[0] == def2[i]:
+        score += lengthMatch(def1, def2, i)
+        print def1, def2, score
     def1 = def1[1:]
-  score += sub_score
   return score
 
-#def bannerScore(syn1, syn2, def1, def2):
-#  def1 = list(def1)
-#  def2 = list(def2)
-#  score = 0
-#  old_score = 1
-#  while old_score != score:
-#    old_score = score
-#    i = 0
-#    j = 0
-#    match_count = 0
-#    for word in def1:
-#      delta_score = 0
-#      while j < len(def2) and delta_score == 0:
-#        if word and word == def2[j]:
-#          delta_score = 1
-#          def2[j] = None
-#          def1[i] = None
-#        else:
-#          j += 1
-#      i += 1
-#      if delta_score == 0 and match_count > 0:
-#        break
-#      elif delta_score == 0:
-#        j = 0
-#      else:
-#        match_count += delta_score * delta_score
-#    score += match_count * match_count
-#  return score 
+def bannerScore1(syn1, syn2, def1, def2):
+  def1 = list(def1)
+  def2 = list(def2)
+  score = 0
+  old_score = 1
+  while old_score != score:
+    old_score = score
+    i = 0
+    j = 0
+    match_count = 0
+    for word in def1:
+      delta_score = 0
+      while j < len(def2) and delta_score == 0:
+        if word and word == def2[j]:
+          delta_score = 1
+          def2[j] = None
+          def1[i] = None
+        else:
+          j += 1
+      i += 1
+      if delta_score == 0 and match_count > 0:
+        break
+      elif delta_score == 0:
+        j = 0
+      else:
+        match_count += delta_score * delta_score
+    score += match_count * match_count
+  return score 
 
 def leskSim(syn1, syn2, def_dict):
   def1 = def_dict[syn1]
@@ -105,3 +102,8 @@ def leskSim(syn1, syn2, def_dict):
       score += 1
   return score
 
+if __name__ == "__main__":
+  def1 = ["the", "cat", "loves", "dogs"]
+  def2 = ["the", "dogs", "cat", "loves", "the"]
+  print bannerScore(None, None, def1, def2)
+  print bannerScore1(None, None, def1, def2)
