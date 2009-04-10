3f4e4e22c2812a6eb02d33a2f1432cb59702edb9
David.Jurgens <David.Jurgens@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-04-10 23:16:50 +0000

    A    sspace/src/sspace/tools
    -  Added tools package for scripts and other programs that operate on the output
    
    A    sspace/src/sspace/tools/SimilarityListGenerator.java
    
    - Added a program to generate lists of most-similar words for each word.
    
    M    sspace/src/sspace/randomindexing/RandomIndexing.java
    
    - Upgraded RI to use the VectorIO and Similarity class
    
    M    sspace/src/sspace/common/Similarity.java
    
    - Added euclideanDistance
    
    A    sspace/src/sspace/common/VectorIO.java
    
    - Added new class to uniformly handle reading and writing of vectors
    
    A    sspace/src/sspace/common/Pair.java
    
    - Added a generic class for representing pairs of object

--- trunk/sspace/src/sspace/common/Similarity.java
+++ trunk/sspace/src/sspace/common/Similarity.java
@@ -30,9 +30,9 @@ public class Similarity {
 	if (a.length != b.length) {
 	    return -1;
 	}
-	long dotProduct = 0.0;
-	long aMagnitude = 0.0;
-	long bMagnitude = 0.0;
+	long dotProduct = 0;
+	long aMagnitude = 0;
+	long bMagnitude = 0;
 	for (int i = 0; i < b.length ; i++) {
 	    int aValue = a[i];
 	    int bValue = b[i];
@@ -40,13 +40,14 @@ public class Similarity {
 	    bMagnitude += bValue * bValue;
 	    dotProduct += aValue * bValue;
 	}
-	aMagnitude = Math.sqrt(aMagnitude);
-	bMagnitude = Math.sqrt(bMagnitude);
-	return dotProduct / (aMagnitude * bMagnitude);
+	
+	double aMagnitudeSqRt = Math.sqrt(aMagnitude);
+	double bMagnitudeSqRt = Math.sqrt(bMagnitude);
+	return dotProduct / (aMagnitudeSqRt * bMagnitudeSqRt);
     }
 
     // REMINDER: this could be made more effecient by not looping
-    private static double computeCorrelation(double[] arr1, double[] arr2) {
+    public static double correlation(double[] arr1, double[] arr2) {
 	double xSum = 0;
 	double ySum = 0;
 	for (int i = 0; i < arr1.length; ++i) {
@@ -69,7 +70,7 @@ public class Similarity {
     }
 
     // REMINDER: this could be made more effecient by not looping
-    private static double computeCorrelation(int[] arr1, int[] arr2) {
+    public static double correlation(int[] arr1, int[] arr2) {
 	long xSum = 0;
 	long ySum = 0;
 	for (int i = 0; i < arr1.length; ++i) {
@@ -90,5 +91,32 @@ public class Similarity {
 	}
 	return numerator / Math.sqrt(xSqSum * ySqSum);
     }
+
+    public static double euclideanDistance(double[] a, double[] b) {
+	if (a == null || b == null || a.length != b.length)
+	    throw new IllegalArgumentException("a: " + a + "; b: " + b);
+	
+	double sum = 0;
+	for (int i = 0; i < a.length; ++i) {
+	    double d = a[i];
+	    double e = b[i];
+	    sum += (d - e) * (d - e);
+	}
+	return Math.sqrt(sum);
+    }
+
+    public static double euclideanDistance(int[] a, int[] b) {
+	if (a == null || b == null || a.length != b.length)
+	    throw new IllegalArgumentException("a: " + a + "; b: " + b);
+	
+	long sum = 0;
+	for (int i = 0; i < a.length; ++i) {
+	    int d = a[i];
+	    int e = b[i];
+	    sum += (d - e) * (d - e);
+	}
+	return Math.sqrt(sum);
+    }
+
 
 }
