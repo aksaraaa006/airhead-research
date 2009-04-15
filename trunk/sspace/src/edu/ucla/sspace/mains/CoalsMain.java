5c59e0d03351a341e541f117577c7a896fbd54d3
FozzietheBeat <FozzietheBeat@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-04-15 05:45:57 +0000

    coals is now mostly fixed, a few issues need to be taken care of.

--- trunk/sspace/src/edu/ucla/sspace/mains/CoalsMain.java
+++ trunk/sspace/src/edu/ucla/sspace/mains/CoalsMain.java
@@ -5,7 +5,7 @@ import edu.ucla.sspace.coals.Coals;
 class CoalsMain {
   public static void main(String[] args) {
     Coals coals = new Coals();
-    String testDocument = "How much wood would a woodchuck chuck , if a woodchuck could chuck wood ? As much wood as a woodchuck would, if a woodchuck could chuck wood";
+    String testDocument = "How much wood would a woodchuck chuck , if a woodchuck could chuck wood ? As much wood as a woodchuck would , if a woodchuck could chuck wood .";
     coals.parseDocument(testDocument);
     coals.processSpace();
     coals.printCorrelations();
