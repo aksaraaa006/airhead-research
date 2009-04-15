08798ea40f77cfc9c49cb7c5f9058f75ee13db63
FozzietheBeat <FozzietheBeat@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-04-15 05:45:57 +0000

    coals is now mostly fixed, a few issues need to be taken care of.

--- sspace/src/edu/ucla/sspace/coals/Coals.java
+++ sspace/src/edu/ucla/sspace/coals/Coals.java
@@ -6,8 +6,10 @@ import edu.ucla.sspace.common.SemanticSpace;
 import edu.ucla.sspace.common.StringUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.HashSet;
 
 import Jama.Matrix;
 
@@ -16,24 +18,20 @@ public class Coals implements SemanticSpace {
   private HashMap<String, Integer> wordToIndex;
   Matrix finalCorrelation;
   private ArrayList<String> wordWindow;
-  private int wordCount;
+  private boolean isFilling;
 
   public Coals() {
     correlation = new HashMap<Index, Double>();
     wordToIndex = new HashMap<String, Integer>();
     wordWindow = new ArrayList<String>();
-    wordCount = 0;
+    isFilling = true;
   }
 
   public void parseDocument(String document) {
     String[] text = document.split("\\s");
     for (String word : text) {
-      String cleaned = StringUtils.cleanup(word);
-      if (!wordToIndex.containsKey(cleaned)) {
-        wordToIndex.put(cleaned, wordCount);
-        wordCount++;
-      }
-      wordWindow.add(cleaned);
+      //String cleaned = StringUtils.cleanup(word);
+      wordWindow.add(word.toLowerCase());
       update();
     }
   }
@@ -46,13 +44,35 @@ public class Coals implements SemanticSpace {
   }
 
   private void update() {
-    if (wordWindow.size() < 9)
+    int size = wordWindow.size();
+    if (size < 9 && isFilling && size < 5) {
+      String mainWord = wordWindow.get(size - 1);
+      for (int i = 0; i < size - 2; i++)
+        addIfMissing(new Index(mainWord, wordWindow.get(i)), i+1);
       return;
+    } else if (size < 9 && !isFilling) {
+      if (size <= 5) {
+        String mainWord = wordWindow.get(0);
+        for (int i = 1; i < size; i++)
+          addIfMissing(new Index(mainWord, wordWindow.get(i)), 5-i);
+      } else {
+        String mainWord = wordWindow.get(size - 5);
+        for (int i = 0; i < size-5; i++)
+          addIfMissing(new Index(mainWord, wordWindow.get(i)), i - (size - 5) + 1);
+        for (int i = 5; i < size; i++)
+          addIfMissing(new Index(mainWord, wordWindow.get(i)), 9-i);
+      }
+      return;
+    } else
+      isFilling = false;
+
     String mainWord = wordWindow.get(4);
     for (int i = 0; i < 4; i++)
       addIfMissing(new Index(mainWord, wordWindow.get(i)), i+1);
-    for (int i = 5; i < 9; i++)
+    for (int i = 5; i < size; i++)
       addIfMissing(new Index(mainWord, wordWindow.get(i)), 9-i);
+    if (size >= 9) 
+      wordWindow.remove(0);
   }
 
   public void reduce() {
@@ -63,15 +83,27 @@ public class Coals implements SemanticSpace {
   }
 
   public void processSpace() {
+    HashSet<String> wordSet = new HashSet<String>();
+    for (Index index : correlation.keySet()) {
+      wordSet.add(index.word);
+      wordSet.add(index.document);
+    }
+    String[] keys = wordSet.toArray(new String[0]);
+    Arrays.sort(keys);
+    for (int i = 0; i < keys.length; ++i)
+      wordToIndex.put(keys[i], i);
+    int wordCount = keys.length;
+
     Matrix correl = new Matrix(wordCount, wordCount, 0);
     for (Map.Entry<Index, Double> entry : correlation.entrySet()) {
       Index key = entry.getKey();
       double value = entry.getValue().doubleValue();
-      correl.set(wordToIndex.get(key.word).intValue(),
-                 wordToIndex.get(key.document).intValue(),
-                 value);
+      int index1 = wordToIndex.get(key.word).intValue();
+      int index2 = wordToIndex.get(key.document).intValue();
+      correl.set(index1, index2, value);
     }
-    Normalize.byCorrelation(correl);
+    //Normalize.byCorrelation(correl);
+    /*
     for (int i = 0; i < wordCount; ++i) {
       for (int j = 0; j < wordCount; ++j) {
         if (correl.get(i,j) < 0)
@@ -80,11 +112,12 @@ public class Coals implements SemanticSpace {
           correl.set(i, j, Math.pow(correl.get(i, j), 2));
       }
     }
+    */
     finalCorrelation = correl;
   }
 
   public void printCorrelations() {
-    finalCorrelation.print(8, 2);
+    finalCorrelation.print(5, 2);
     for (Map.Entry<String, Integer> entry : wordToIndex.entrySet())
       System.out.println(entry.getKey() + " has index: " + entry.getValue());
   }
