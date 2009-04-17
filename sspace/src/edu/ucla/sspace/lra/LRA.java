da614ab64384ff8147de7a6756989f72e2707eb1
chippoc <chippoc@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-04-17 07:16:13 +0000

    just adding the LRA.java file. will add more later in the week

---
+++ sspace/src/edu/ucla/sspace/lra/LRA.java
@@ -0,0 +1,33 @@
+//package edu.ucla.sspace.lra;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+import java.util.regex.Pattern;
+
+public class LRA {
+    public LRA() {
+        //add global variables in here
+        ;
+    } 
+
+    public static void main(String[] args) {
+        System.out.println("starting LRA...\n");
+        //Todo: everything
+    }
+}
+
