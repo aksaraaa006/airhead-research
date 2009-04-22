90f8c85c0f3b9273cdee1af9f6a2f5110b80ad62
FozzietheBeat <FozzietheBeat@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-04-22 21:40:30 +0000

    blah

--- trunk/soms/learningGame.py
+++ trunk/soms/learningGame.py
@@ -9,15 +9,29 @@ from learningAgent import learningAgent
 VECTOR_SIZE = 60
 
 class LearningGame():
-  def __init__(self):
+  def __init__(self, phoneme_file):
     self.map = []
     self.objects = []
+    self.word_list = []
+    self.buildWordList(phoneme_file)
     self.createObjects(60)
     self.createLearners(2)
 
+  def buildWordList(self, phoneme_file):
+    phonemes = open(phoneme_file)
+    for line in phonemes:
+      phoneme = line.split()
+      word = phoneme[0]
+      phoneme_values = phoneme[4:]
+      phoneme_rep = numpy.zeros((len(phoneme_values)))
+      for i in range(len(phoneme_values)):
+        phoneme_rep[i] = float(phoneme_values[i])
+      self.word_list.append((word, phoneme_rep))
+        
   def createLearners(self, num_players):
-    self.learners = [learningAgent(VECTOR_SIZE, self) for i in
-                     range(num_players)]
+    phoneme_size = len(self.word_list[0][1])
+    self.learners = [learningAgent(VECTOR_SIZE, phoneme_size, self)
+                     for i in range(num_players)]
 
   def createObjects(self, num_objects):
     for i in range(num_objects):
@@ -31,25 +45,28 @@ class LearningGame():
   def playGame(self, num_times):
     for i in range(num_times):
       [speaker, hearer] = random.sample(self.learners, 2)
-      word, context = speaker.generateUtterance()
+      context, word = speaker.generateUtterance()
       #print "speaker says: ", word, context
-      hearer.recieveUtterance(word, context)
+      hearer.receiveUtterance(word, context)
+
     for learner in self.learners:
-      print "mappings for learner"
-      #learner.printMap()
+      learner.printMap()
 
     converge_count = 0
     for obj in self.objects:
       meanings = set()
       for learner in self.learners:
-        meanings.add(learner.getBestWord(obj))
+        suc, _, word = learner.produceWord(obj, False)
+        if suc:
+          meanings.add(word[0])
+        else:
+          meanings.add("")
       print meanings
       if len(meanings) == 1 and "" not in meanings: 
         converge_count += 1
     print "number of converging mappings %f" %(converge_count /
-                                               len(self.objects))
+                                               float(len(self.objects)))
 
 if __name__ == "__main__":
-  game = LearningGame()
+  game = LearningGame(sys.argv[2])
   game.playGame(int(sys.argv[1]))
-
