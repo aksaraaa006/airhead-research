2fcc4ccc5c424d998506736d7f6c075d8dd61bfc
FozzietheBeat <FozzietheBeat@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-04-26 23:53:04 +0000

    add a document cleaner for one line per doc files

---
+++ sspace/src/edu/ucla/sspace/tools/DocumentCleaner.java
@@ -0,0 +1,28 @@
+package edu.ucla.sspace.tools;
+
+import edu.ucla.sspace.common.DocumentPreprocessor;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class DocumentCleaner {
+  public static void main(String[] args) {
+    try {
+      File wordList = new File(args[1]);
+      DocumentPreprocessor processor = new DocumentPreprocessor(wordList);
+      BufferedReader br = new BufferedReader(new FileReader(args[2]));
+      BufferedWriter bw = new BufferedWriter(new FileWriter(args[3]));
+      for (String line = null; (line = br.readLine()) != null;) {
+        String cleaned = processor.process(line);
+        bw.write(cleaned);
+      }
+      bw.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
