bbae97e840ffc99029b401b0c36cf101141802bd
David.Jurgens <David.Jurgens@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-04-28 00:55:42 +0000

    M    sspace/src/edu/ucla/sspace/tools/SparseMatrixConverter.java M    sspace/src/edu/ucla/sspace/common/ArgOptions.java M    sspace/src/edu/ucla/sspace/common/VectorIO.java M    sspace/src/edu/ucla/sspace/esa/ExplicitSemanticAnalysis.java M    sspace/src/edu/ucla/sspace/lra/LRA.java
    - added copyright header

--- sspace/src/edu/ucla/sspace/common/VectorIO.java
+++ sspace/src/edu/ucla/sspace/common/VectorIO.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright 2009 David Jurgens
+ *
+ * This file is part of the S-Space package and is covered under the terms and
+ * conditions therein.
+ *
+ * The S-Space package is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation and distributed hereunder to you.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND NO REPRESENTATIONS OR WARRANTIES,
+ * EXPRESS OR IMPLIED ARE MADE.  BY WAY OF EXAMPLE, BUT NOT LIMITATION, WE MAKE
+ * NO REPRESENTATIONS OR WARRANTIES OF MERCHANT- ABILITY OR FITNESS FOR ANY
+ * PARTICULAR PURPOSE OR THAT THE USE OF THE LICENSED SOFTWARE OR DOCUMENTATION
+ * WILL NOT INFRINGE ANY THIRD PARTY PATENTS, COPYRIGHTS, TRADEMARKS OR OTHER
+ * RIGHTS.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package edu.ucla.sspace.common;
 
 import java.io.BufferedReader;
@@ -8,6 +29,8 @@ import java.io.PrintWriter;
 
 /**
  * A shared utility for printing vectors to files in a uniform manner.
+ *
+ * @author David Jurgens
  */
 public class VectorIO {
     
@@ -25,12 +48,6 @@ public class VectorIO {
 	}
 	br.close();
 	return values;
-    }
-
-    public static void writeVector(int[] vector, PrintWriter pw) 
-	throws IOException {
-	pw.println(toString(vector));
-	pw.close();
     }
 
     public static String toString(double[] vector) {
@@ -51,6 +68,13 @@ public class VectorIO {
 	sb.append(vector[vector.length-1]);
 	
 	return sb.toString();
+    }
+
+
+    public static void writeVector(int[] vector, PrintWriter pw) 
+	throws IOException {
+	pw.println(toString(vector));
+	pw.close();
     }
     
     public static void writeVector(int[] vector, File f)
--- sspace/src/edu/ucla/sspace/tools/SparseMatrixConverter.java
+++ sspace/src/edu/ucla/sspace/tools/SparseMatrixConverter.java
@@ -19,7 +19,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package edu.ucla.sspace.lsa;
+package edu.ucla.sspace.tools;
 
 import java.io.*;
 import java.util.*;
