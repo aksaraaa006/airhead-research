50e9076f9e8af58612f6b8963dba47dce627c38b
David.Jurgens <David.Jurgens@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-04-30 20:04:01 +0000

    Javadoc + performance tweaks
    M    sspace/src/edu/ucla/sspace/common/Matrix.java
    
    - Added javadoc
    
    M    sspace/src/edu/ucla/sspace/common/matrix/OnDiskMatrix.java
    
    - Converted getRow() to use a byte[] call, which should improve I/O performance
      since we are reading a page at a time on disk.
    
    - Removed File/String constructors because it is unnecessary for anyone to
      provide their own file to back the matrix.
    
    - Added Javadoc

--- sspace/src/edu/ucla/sspace/common/Matrix.java
+++ sspace/src/edu/ucla/sspace/common/Matrix.java
@@ -30,57 +30,64 @@ package edu.ucla.sspace.common;
 public interface Matrix {
 
     /**
-     *
+     * The type of matrix instance.  This enum should be used as a hint for
+     * algorithms that create {@code Matrix} instances as to what implementation
+     * to use.
      */
     public enum Type {
 	/**
-	 *
+	 * A matrix where the majority of the values are 0, and is
+	 * small enough to fit into memory.
 	 */
 	SPARSE_IN_MEMORY,
 
 	/**
-	 *
+	 * A matrix that contains very few 0 values and is small
+	 * enough to fit into memory.
 	 */
 	DENSE_IN_MEMORY,
 
 	/**
-	 *
+	 * A matrix with very few non-zero values and is sufficiently large
+	 * enough that it would not fit in memory.
 	 */
 	SPARSE_ON_DISK,
 
 	/**
-	 *
+	 * A matrix with very few zero values and is sufficiently large enough
+	 * that it would not fit in memory.
 	 */
 	DENSE_ON_DISK	    
     }
 
     /**
-     *
+     * Returns the value of the matrix at the provided row and column.
      */
     double get(int row, int col);
 
     /**
-     *
+     * Returns the entire row.
      */
     double[] getRow(int row);
 
     /**
-     *
+     * Returns the number of columns in this matrix.
      */
     int columns();
     
     /**
-     *
+     * Converts the matrix to a two dimensional array.  Note that for large
+     * matrices, this may exhaust all available memory.
      */
     double[][] toDenseArray();
 
     /**
-     *
+     * Returns the number of rows in this matrix.
      */
     int rows();    
 
     /**
-     *
+     * Sets the location at the row and column to the provided value.
      */
     void set(int row, int col, double val);
 
