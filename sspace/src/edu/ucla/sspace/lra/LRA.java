62fe440ad8b1e418a244ad5e53a91a50bcf6a6c1
chippoc <chippoc@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-05-10 20:55:32 +0000

    encapsulation of filtering process in its own function.

--- sspace/src/edu/ucla/sspace/lra/LRA.java
+++ sspace/src/edu/ucla/sspace/lra/LRA.java
@@ -21,6 +21,7 @@
 
 package edu.ucla.sspace.lra;
 
+import edu.ucla.sspace.common.Pair;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -31,19 +32,22 @@ import java.io.PrintWriter;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+import java.lang.Float;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.HashSet;
+//import java.util.TreeMap;
 import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 import java.util.Scanner;
-
-import java.util.regex.Pattern;
+import java.util.SortedMap;
 
 import edu.ucla.sspace.common.Index;
+import edu.ucla.sspace.common.HashMultiMap;
 
 import edu.smu.tspell.wordnet.*;
 
@@ -62,18 +66,21 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.PhraseQuery;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.Searcher;
+import org.apache.lucene.search.Similarity;
+
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
-
 public class LRA {
     //constants...should probably be in the constructor
     private static final int NUM_SIM = 10; 
     private static final int MAX_PHRASE = 5; 
+    private static final int NUM_FILTER = 3;
     private static final String INDEX_DIR = "/home/chippoc/index/"; 
-    private static final String DATA_DIR = "/bigdisk/corpora/childrens-books/"; 
+    private static final String DATA_DIR = "/bigdisk/corpora/textbooks/";//"/bigdisk/corpora/usenet/maui.tapor.ualberta.ca:9000/newscorpus/";
+    private static final boolean DO_INDEX = false;
 
     public LRA() {
         //constructor...will fill in later
@@ -205,15 +212,37 @@ public class LRA {
         long start = new Date().getTime();
         System.out.println("counting phrases " + A + ":" + B);
         QueryParser parser = new QueryParser("contents",new StandardAnalyzer());
-        System.out.println("nsearching for: '\"" + A + " " + B + "\"~"+MAX_PHRASE+"'");
+        System.out.println("searching for: '\"" + A + " " + B + "\"~"+MAX_PHRASE+"'");
         parser.setPhraseSlop(MAX_PHRASE);
         String my_phrase = "\"" + A + " " + B + "\"";
         Query query = parser.parse(my_phrase);
-        TopDocs results = searcher.search(query,10);
         //System.out.println("total hits: " + results.totalHits);
 
-        //TODO: set similarity to use only the frequencies
-        //searcher.setSimilarity(/*similarity*/);
+        //set similarity to use only the frequencies
+        //score is based on frequency of phrase only
+        searcher.setSimilarity(
+                new Similarity() {
+                   public static final long serialVersionUID = 1L;
+                   public float coord(int overlap, int maxOverlap) {
+                      return 1;
+                   } 
+                   public float queryNorm(float sumOfSquaredWeights) {
+                      return 1;
+                   } 
+                   public float tf(float freq) {
+                      return freq;
+                   } 
+                   public float idf(int docFreq, int numDocs) {
+                      return 1;
+                   } 
+                   public float lengthNorm(String fieldName, int numTokens) {
+                      return 1;
+                   } 
+                   public float sloppyFreq(int distance) {
+                       return 1;
+                   }
+        });
+        TopDocs results = searcher.search(query,10);
 
         ScoreDoc[] hits = results.scoreDocs;
         float total_score = 0;
@@ -231,6 +260,128 @@ public class LRA {
         return total_score;
     }
 
+    /**
+     * Returns an ArrayList of phrases with the greatest frequencies in the corpus.
+     * For each alternate pair, send a phrase query to the Lucene search engine
+     * containing the corpus.  The phrase query will find the frequencies of phrases
+     * that begin with one member of the pair and end with the other.  The phrases
+     * cannot have more than MAX_PHRASE words.
+     * Select the top NUM_FILTER (current NUM_FILTER=3) most frequent phrases and 
+     * return them along with the original pairs.
+     *
+     * @param A a String containing the first member in the original pair 
+     * @param B a String containing the second member in the original pair 
+     * @param A_prime a Synset array containing the alternates for A 
+     * @param B_prime a Synset array containing the alternates for B 
+     * @return  an ArrayList of Strings with the top NUM_FILTER pairs along with the original pairs 
+     */
+    public static ArrayList<String> filterPhrases (String A, String B, Synset[] A_prime, Synset[] B_prime) {
+        HashMultiMap<Float,Pair<String>> phrase_frequencies  = new HashMultiMap<Float,Pair<String>>();
+        //Search corpus... A:B
+        //phrase_frequencies.put(new Float(countPhraseFrequencies(INDEX_DIR, A, B)),new Pair<String>(A,B)); 
+        //System.out.println("Top 10 Similar words:");
+        System.out.print("A: ");
+        int count = 0;
+        for (int i = 0; (i < NUM_SIM && i < A_prime.length); i++) {
+            String[] wordForms = A_prime[i].getWordForms();
+            for (int j = 0; j < wordForms.length; j++)
+            {
+                if (wordForms[j].compareTo(A) != 0) {
+                    //Search corpus... A':B
+                    Float score = new Float(countPhraseFrequencies(INDEX_DIR, wordForms[j], B));
+                    System.out.println("score: " + score); 
+                    phrase_frequencies.put(score,new Pair<String>(wordForms[j],B)); 
+                    //System.out.print((j > 0 ? ", " : "") +
+                    //   wordForms[j]);
+                    count++;
+                }
+
+                if(count >= NUM_SIM)
+                    break;
+            }
+            if(count >= NUM_SIM)
+                break;
+        }
+        System.out.print("\n");
+        System.out.print("B: ");
+        count = 0;
+        for (int i = 0; (i < NUM_SIM && i < B_prime.length); i++) {
+            String[] wordForms = B_prime[i].getWordForms();
+            for (int j = 0; j < wordForms.length; j++)
+            {
+                if (wordForms[j].compareTo(B) != 0) {
+                    //Search corpus... A:B'
+                    Float score = new Float(countPhraseFrequencies(INDEX_DIR,A, wordForms[j]));
+                    System.out.println("score: " + score); 
+                    phrase_frequencies.put(score,new Pair<String>(A,wordForms[j])); 
+                    //System.out.print((j > 0 ? ", " : "") +
+                    //   wordForms[j]);
+                    count++;
+                }
+
+                if(count >= NUM_SIM)
+                    break;
+            }
+            if(count >= NUM_SIM)
+                break;
+        }
+        System.out.print("\n");
+        
+        // filter out the phrases and add the top 3 to the ArrayList, and return it
+        Iterator iter = phrase_frequencies.keySet().iterator();
+        //TODO: make number of filters dynamic
+        //create Array with size = num filters
+        ArrayList<String> filtered_phrases = new ArrayList<String>();
+        Float filter1 = new Float(0.0);
+        Float filter2 = new Float(0.0); 
+        Float filter3 = new Float(0.0);
+        while (iter.hasNext()) {
+            Float curr_key = (Float)iter.next();
+            //this will bump the filters up each time a greater value comes along
+            //so that filter1 will be the greatest key and filter3 the 3rd greatest
+            if (curr_key > filter1) {
+                filter3 = filter2;
+                filter2 = filter1; 
+                filter1 = curr_key;
+            } else if (curr_key > filter2) {
+                filter3 = filter2;
+                filter2 = curr_key;
+            } else if (curr_key > filter3) {
+                filter3 = curr_key;
+            }
+            Iterator val_iter = phrase_frequencies.get(curr_key).iterator();
+            while (val_iter.hasNext()) {
+                System.out.println("key: " + curr_key + ", val: " + val_iter.next());
+            }
+
+        }
+        System.out.println("filter1: " + filter1 + "\n" + "filter2: " + filter2 + "\n" + "filter3: " + filter3);
+        int filter_count = 0;
+        Iterator val_iter = phrase_frequencies.get(filter1).iterator();
+        while (val_iter.hasNext() && filter_count < 3) {
+            filtered_phrases.add(val_iter.next().toString());
+            filter_count++;
+        }
+        val_iter = phrase_frequencies.get(filter2).iterator();
+        while (val_iter.hasNext() && filter_count < 3) {
+            filtered_phrases.add(val_iter.next().toString());
+            filter_count++;
+        }
+        val_iter = phrase_frequencies.get(filter3).iterator();
+        while (val_iter.hasNext() && filter_count < 3) {
+            filtered_phrases.add(val_iter.next().toString());
+            filter_count++;
+        }
+        //throw in the original pairs also
+        filtered_phrases.add("{"+A+", "+B+"}");
+        filtered_phrases.add("{"+B+", "+A+"}");
+
+        System.out.println("filtered phrases: " + filtered_phrases);
+
+        return filtered_phrases;
+        
+    }
+
     public static void main(String[] args) {
         //set system property for Wordnet database directory
         Properties sysProps = System.getProperties();
@@ -245,53 +396,17 @@ public class LRA {
         String B = sc.next(); 
 
         //Index corpus...
-        initializeIndex(INDEX_DIR, DATA_DIR);
+        if ( DO_INDEX ) {
+            initializeIndex(INDEX_DIR, DATA_DIR);
+        } else {
+            System.out.println("skipping indexing step...");
+        }
 
         //1. Find alternates for A and B
         Synset[] A_prime = findAlternatives(A);
         Synset[] B_prime = findAlternatives(B);
-
-        //Search corpus... A:B
-        countPhraseFrequencies(INDEX_DIR, A, B); 
-
-        //System.out.println("Top 10 Similar words:");
-        System.out.print("A: ");
-        int count = 0;
-        for (int i = 0; (i < NUM_SIM && i < A_prime.length); i++) {
-            String[] wordForms = A_prime[i].getWordForms();
-            for (int j = 0; j < wordForms.length; j++)
-            {
-                //Search corpus... A':B
-                System.out.println("score: " + countPhraseFrequencies(INDEX_DIR, wordForms[j], B)); 
-                //System.out.print((j > 0 ? ", " : "") +
-                 //   wordForms[j]);
-                count++;
-                if(count >= NUM_SIM)
-                    break;
-            }
-            if(count >= NUM_SIM)
-                break;
-        }
-        System.out.print("\n");
-        System.out.print("B: ");
-        count = 0;
-        for (int i = 0; (i < NUM_SIM && i < B_prime.length); i++) {
-            String[] wordForms = B_prime[i].getWordForms();
-            for (int j = 0; j < wordForms.length; j++)
-            {
-                //Search corpus... A:B'
-                System.out.println("score: " + countPhraseFrequencies(INDEX_DIR, A, wordForms[j])); 
-                //System.out.print((j > 0 ? ", " : "") +
-                 //   wordForms[j]);
-                count++;
-                if(count >= NUM_SIM)
-                    break;
-            }
-            if(count >= NUM_SIM)
-                break;
-        }
-        System.out.print("\n");
-        /*end: testing similar words...*/
+        
+        ArrayList<String> filtered_phrases = filterPhrases(A, B, A_prime, B_prime);
 
     }
 }
