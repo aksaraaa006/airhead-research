a200f3f6170a46f85d145a5a51142975ace10cef
FozzietheBeat <FozzietheBeat@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-05-12 00:13:57 +0000

    reformulate the sparse matrix to hopefully save some space.

--- sspace/src/edu/ucla/sspace/common/matrix/SparseMatrix.java
+++ sspace/src/edu/ucla/sspace/common/matrix/SparseMatrix.java
@@ -21,136 +21,170 @@
 
 package edu.ucla.sspace.common.matrix;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 import edu.ucla.sspace.common.Matrix;
 
 /**
- * A sparse {@code Matrix} backed by a {@link Map}.
- *
+ * A sparse {@code Matrix} based on the Yale Sparse Matrix Format.
+ * Each row is allocated a linked list structure which keeps the non-zero column
+ * values in column order.  Lookups are O(log n) where n is the number of
+ * non-zero values for the largest row.  Inserting is currently dependent on the
+ * insertion time of an ArrayList.
  *
  * @author David Jurgens
  */
 public class SparseMatrix implements Matrix {
     
-    private final int rows;
+  private final int rows;
 
-    private final int cols;
-    
-    private final Map<Index,Double> sparseMatrix;
+  private final int cols;
+  
+  /**
+   * Each row is defined as a {@link RowEntry} which does most of the work.
+   */
+  private final RowEntry[] sparseMatrix;
+
+  /**
+   */
+  public SparseMatrix(int rows, int cols) {
+    this.rows = rows;
+    this.cols = cols;
+    sparseMatrix = new RowEntry[rows];
+    for (int i = 0; i < rows; ++i)
+      sparseMatrix[i] = new RowEntry();
+  }
+
+  /**
+   *
+   */    
+  private void checkIndices(int row, int col) {
+    if (row < 0 || col < 0 || row >= rows || col >= cols) {
+      throw new ArrayIndexOutOfBoundsException();
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public double get(int row, int col) {
+    return sparseMatrix[row].getValue(col);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public double[] getRow(int row) {
+    return sparseMatrix[row].getRow(cols);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public int columns() {
+    return cols;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public void set(int row, int col, double val) {
+    sparseMatrix[row].setValue(col, val);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public void setRow(int row, double[] columns) {
+    if (columns.length != cols) {
+      throw new IllegalArgumentException(
+      "invalid number of columns: " + columns.length);
+    }
+    for (int col = 0; col < cols; ++col) {
+      sparseMatrix[row].setValue(col, columns[col]);
+    }
+  }
+  
+  /**
+   * {@inheritDoc}
+   */
+  public double[][] toDenseArray() {
+    double[][] m = new double[rows][cols];
+    for (int r = 0; r < rows; ++r) {
+      m[r] = sparseMatrix[r].getRow(cols);
+    }
+    return m;
+  }
+
+  /**
+   *
+   */
+  public int rows() {
+    return rows;
+  }
+
+  public static final class RowEntry {
+    /**
+     * An arraylist of non zero values for this row, stored in the correct
+     * column order.
+     */
+    private ArrayList<Double> values;
+    /**
+     * An arraylist of which column indexes are stored for this row in sorted
+     * order.
+     */
+    private ArrayList<Integer> columnIndexes;
 
     /**
-     * Create a matrix of the provided size using a temporary file.
+     * Create the two lists, with zero values in them initially.
      */
-    public SparseMatrix(int rows, int cols) {
-
-	this.rows = rows;
-	this.cols = cols;
-	sparseMatrix = new HashMap<Index,Double>();
+    public RowEntry() {
+      values = new ArrayList<Double>();
+      columnIndexes = new ArrayList<Integer>();
     }
 
     /**
+     * retrieve the value at specified column
+     * @param column The column value to get
+     * @return the value for the specified column, or 0 if no column is found.
+     */
+    public double getValue(int column) {
+      int valueIndex = Collections.binarySearch(columnIndexes, column);
+      return (valueIndex >= 0) ? values.get(valueIndex) : 0.0;
+    }
+
+    /**
+     * Update the RowEntry such that the index at column now stores value.  If
+     * value is 0, this will remove the column from the row entry for efficency.
      *
-     */    
-    private void checkIndices(int row, int col) {
-	if (row < 0 || col < 0 || row >= rows || col >= cols) {
-	    throw new ArrayIndexOutOfBoundsException();
-	}
+     * @param column The column index this value should be stored as
+     * @param value The value to store
+     */
+    public void setValue(int column, double value) {
+      int valueIndex = Collections.binarySearch(columnIndexes, column);
+      if (valueIndex >= 0 && value > 0.0) {
+        values.set(valueIndex, value);
+      } else if (value > 0.0) {
+        values.add((valueIndex + 1) * -1, value);
+        columnIndexes.add((valueIndex+1) * -1, column);
+      } else if (valueIndex >= 0) {
+        values.remove(valueIndex);
+        columnIndexes.remove(valueIndex);
+      }
     }
 
     /**
-     * {@inheritDoc}
+     * A dense double array which this RowEntry represents.
      */
-    public double get(int row, int col) {
-	Double d = sparseMatrix.get(new Index(row, col));
-	return (d == null) ? 0d : d.doubleValue();
+    public double[] getRow(int columnSize) {
+      double[] dense = new double[columnSize];
+      for (int i = 0; i < columnIndexes.size(); ++i) {
+        dense[columnIndexes.get(i).intValue()] = values.get(i).doubleValue();
+      }
+      return dense;
     }
-
-    /**
-     * {@inheritDoc}
-     */
-    public double[] getRow(int row) {
-	double[] rowArr = new double[cols];
-
-	for (int i = 0; i < cols; ++i) {
-	    rowArr[i] = get(row, i);
-	}
-	return rowArr;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public int columns() {
-	return cols;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public void set(int row, int col, double val) {
-	sparseMatrix.put(new Index(row, col), val);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public void setRow(int row, double[] columns) {
-	if (columns.length != cols) {
-	    throw new IllegalArgumentException(
-		"invalid number of columns: " + columns.length);
-	}
-	for (int col = 0; col < cols; ++col) {
-	    sparseMatrix.put(new Index(row, col), columns[col]);
-	}
-    }
-    
-    /**
-     * {@inheritDoc}
-     */
-    public double[][] toDenseArray() {
-	double[][] m = new double[rows][cols];
-	int i = 0;
-	for (int row = 0; row < rows; ++row) {
-	    for (int col = 0; col < cols; ++col) {
-		m[row][col] = get(row, col);
-	    }
-	}
-	return m;
-    }
-
-    /**
-     *
-     */
-    public int rows() {
-	return rows;
-    }
-
-    /**
-     *
-     */
-    private static final class Index {
-
-	final int row;
-	final int col;
-
-	public Index(int row, int col) {
-	    this.row = row;
-	    this.col = col;
-	}
-
-	public boolean equals(Object o) {
-	    if (o instanceof Index) {
-		Index i = (Index)o;
-		return i.row == row && i.col == col;
-	    }
-	    return false;
-	}
-
-	public int hashCode() {
-	    return row << 16 | col;
-	}
-    }
-
+  }
 }
