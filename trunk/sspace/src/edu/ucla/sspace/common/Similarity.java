bdeec7c66410490ca1c6d65916ed5e740142799c
David.Jurgens <David.Jurgens@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-05-13 08:47:26 +0000

    Added Spearman Rank Correlation Coefficient and some more Javadoc

--- trunk/sspace/src/edu/ucla/sspace/common/Similarity.java
+++ trunk/sspace/src/edu/ucla/sspace/common/Similarity.java
@@ -23,17 +23,25 @@ package edu.ucla.sspace.common;
 
 import java.lang.reflect.Method;
 
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 /**
  * A collection of static methods for computing the similarity between different
- * vectors.  Semantic space implementations should use this class.
+ * vectors.  {@link SemanticSpace} implementations should use this class.
  */
 public class Similarity {
     
-  public enum SimType {
-    COSINE,
-    CORRELATION,
-    EUCLIDEAN,
-  }
+    /**
+     * A type of similarity function to use when generating a {@link Method}
+     */
+    public enum SimType {
+	COSINE,
+	PEARSON_CORRELATION,
+	EUCLIDEAN,
+        SPEARMAN_RANK_CORRELATION
+    }
 
     /**
      * Uninstantiable
@@ -54,7 +62,7 @@ public class Similarity {
 	case COSINE:
 	    methodName = "cosineSimilarity";
 	    break;
-	case CORRELATION:
+	case PEARSON_CORRELATION:
 	    methodName = "correlation";
 	    break;
 	case EUCLIDEAN:
@@ -62,11 +70,11 @@ public class Similarity {
 	    break;
 	}
 	
-	Method m = null;
+ 	Method m = null;
 
 	try { 
 	    m = Similarity.class.getMethod(methodName,
-		new Class[] {double[].class, double[].class});
+					   new Class[] {double[].class, double[].class});
 	} catch (NoSuchMethodException nsme) {
 	    // rethrow
 	    throw new Error(nsme);
@@ -75,10 +83,33 @@ public class Similarity {
 	return m;
     }
 
-    public static double cosineSimilarity(double[] a, double[] b) {
+    /**
+     * Throws an exception if either array is {@code null} or if the array
+     * lengths do not match.
+     */
+    private static void check(double[] a, double[] b) {
 	if (a.length != b.length) {
-	    return -1;
+	    throw new IllegalArgumentException(
+		"input array lengths do not match");
 	}
+    }
+
+    /**
+     * Throws an exception if either array is {@code null} or if the array
+     * lengths do not match.
+     */
+    private static void check(int[] a, int[] b) {
+	if (a.length != b.length) {
+	    throw new IllegalArgumentException(
+		"input array lengths do not match");
+	}
+    }
+    
+    /**
+     * Returns the cosine similarity of the two arrays.
+     */
+    public static double cosineSimilarity(double[] a, double[] b) {
+	check(a,b);
 	double dotProduct = 0.0;
 	double aMagnitude = 0.0;
 	double bMagnitude = 0.0;
@@ -96,6 +127,9 @@ public class Similarity {
 	    : dotProduct / (aMagnitude * bMagnitude);
     }
     
+    /**
+     * Returns the cosine similarity of the two arrays.
+     */
     public static double cosineSimilarity(int[] a, int[] b) {
 	if (a.length != b.length) {
 	    return -1;
@@ -118,8 +152,12 @@ public class Similarity {
 	    : dotProduct / (aMagnitude * bMagnitude);
     }
 
-    // REMINDER: this could be made more effecient by not looping
+    /**
+     * Returns the Pearson product-moment correlation coefficient of the two
+     * arrays.
+     */
     public static double correlation(double[] arr1, double[] arr2) {
+	// REMINDER: this could be made more effecient by not looping
 	double xSum = 0;
 	double ySum = 0;
 	for (int i = 0; i < arr1.length; ++i) {
@@ -141,8 +179,12 @@ public class Similarity {
 	return numerator / Math.sqrt(xSqSum * ySqSum);
     }
 
-    // REMINDER: this could be made more effecient by not looping
+    /**
+     * Returns the Pearson product-moment correlation coefficient of the two
+     * arrays.
+     */
     public static double correlation(int[] arr1, int[] arr2) {
+	// REMINDER: this could be made more effecient by not looping
 	long xSum = 0;
 	long ySum = 0;
 	for (int i = 0; i < arr1.length; ++i) {
@@ -191,10 +233,92 @@ public class Similarity {
     }
 
     public static double euclideanSimilarity(int[] a, int[] b) {
-      return 1 / (1 + euclideanDistance(a,b));
+	return 1 / (1 + euclideanDistance(a,b));
     }
 
     public static double euclideanSimilarity(double[] a, double[] b) {
-      return 1 / (1 + euclideanDistance(a,b));
+	return 1 / (1 + euclideanDistance(a,b));
     }
+
+    /**
+     * Computes the Spearman rank correlation coefficient for the two arrays.
+     * If there is a tie in the ranking of {@code a}, then Pearson's
+     * product-moment coefficient is returned instead.
+     */
+    public static double spearmanRankCorrelationCoefficient(double[] a, 
+							    double[] b) {
+	check(a,b);
+	SortedMap<Double,Double> ranking = new TreeMap<Double,Double>();
+	for (int i = 0; i < a.length; ++i) {
+	    ranking.put(a[i], b[i]);
+	}
+	
+	// keep track of the last value we saw in the key set so we can check
+	// for ties.  If there are ties then the Pearson's product-moment
+	// coefficient should be returned instead.
+	Double last = null;
+
+	double diff = 0d;
+
+	for (Map.Entry<Double,Double> e : ranking.entrySet()) {
+	    Double x = e.getKey();
+	    Double y = e.getValue();
+	    // check that there are no tied rankings
+	    if (last == null) {
+		last = x;
+	    }
+	    // if there was a tie, return the correlation instead.
+	    else if (last.equals(x)) {
+		return correlation(a,b);
+	    }
+	    else {
+		last = x;
+	    }
+
+	    double d = x.doubleValue() - y.doubleValue();
+	    diff += d * d;
+	}
+	return 1 - ((6 * diff) / (a.length * (a.length * a.length - 1)));
+    }
+
+    /**
+     * Computes the Spearman rank correlation coefficient for the two arrays.
+     * If there is a tie in the ranking of {@code a}, then Pearson's
+     * product-moment coefficient is returned instead.
+     */
+    public static double spearmanRankCorrelationCoefficient(int[] a, int[] b) {
+	check(a,b);
+	SortedMap<Integer,Integer> ranking = new TreeMap<Integer,Integer>();
+	for (int i = 0; i < a.length; ++i) {
+	    ranking.put(a[i], b[i]);
+	}
+	
+	// keep track of the last value we saw in the key set so we can check
+	// for ties.  If there are ties then the Pearson's product-moment
+	// coefficient should be returned instead.
+	Integer last = null;
+
+	long diff = 0l;
+
+	for (Map.Entry<Integer,Integer> e : ranking.entrySet()) {
+	    Integer x = e.getKey();
+	    Integer y = e.getValue();
+	    // check that there are no tied rankings
+	    if (last == null) {
+		last = x;
+	    }
+	    // if there was a tie, return the correlation instead.
+	    else if (last.equals(x)) {
+		return correlation(a,b);
+	    }
+	    else {
+		last = x;
+	    }
+
+	    long d = x.longValue() - y.longValue();
+	    diff += d * d;
+	}
+	return 1 - ((6d * diff) / (a.length * (a.length * a.length - 1d)));
+    }
+
 }
