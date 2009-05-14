ce57e6d750cad3c528475f5dc94c4950300dbb61
FozzietheBeat <FozzietheBeat@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-05-14 21:10:19 +0000

    add random mapping to feature maker

--- soms/feature_maker.py
+++ soms/feature_maker.py
@@ -21,31 +21,24 @@ def buildFeatureSet(keys, features, word_file):
   vector_size = max(feature_values.values()) + 1
   word_features = []
   word_mappings = {}
-  line_count = 0
-  for line in features.readlines():
-    feature_vector = [0 for i in range(vector_size)] 
+  for line_count, line in enumerate(features.readlines()):
+    feature_vector = numpy.zeros((vector_size)) 
     words = line.split()
     word_mappings[words[0]] = line_count 
-    line_count += 1
     for word in words[1:]:
       feature_vector[feature_values[word]] = 1
     word_features.append(feature_vector)
 
   print [(word, word_mappings[word[0]]) for word in word_list if word[0] in
          word_mappings]
-  # Save the word, and it's feature vector for all words in the word list.
-  used_words = [(word, numpy.array(word_features[word_mappings[word[0]]]))
-                for word in word_list if word[0] in word_mappings]
+  r = numpy.random.rand(vector_size, 100)
+  used_words = []
+  for word in word_list:
+    if word[0] in word_mappings:
+      feature_vector = word_features[word_mappings[word[0]]]
+      used_words.append((word, numpy.dot(feature_vector, r)))
   pickle.dump(used_words, open("object_features", 'w'))
   return
-
-  # Run the svd on all the feature vectors to reduce the dimensionality to 100
-  # features.
-  feature_matrix = numpy.array(word_features)
-  size = len(word_features)
-  u,s,v = numpy.linalg.svd(feature_matrix)
-  u = u[numpy.ix_(range(size), range(100))]
-
 
 if __name__ == "__main__":
   if len(sys.argv) != 4:
