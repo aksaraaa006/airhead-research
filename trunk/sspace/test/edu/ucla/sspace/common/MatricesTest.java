c6f797739ad3fc435b26b87a1eecb80067ddd729
FozzietheBeat <FozzietheBeat@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-05-18 22:29:25 +0000

    add a new growing sparse matrix add a naive matrix multiplication implementation with tests for both

---
+++ trunk/sspace/test/edu/ucla/sspace/common/MatricesTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2009 Keith Stevens 
+ *
+ * This file is part of the S-Space package and is covered under the terms and
+ * conditions therein.
+ *
+ * The S-Space package is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation and distributed hereunder to you.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND NO REPRESENTATIONS OR WARRANTIES,
+ * EXPRESS OR IMPLIED ARE MADE.  BY WAY OF EXAMPLE, BUT NOT LIMITATION, WE MAKE
+ * NO REPRESENTATIONS OR WARRANTIES OF MERCHANT- ABILITY OR FITNESS FOR ANY
+ * PARTICULAR PURPOSE OR THAT THE USE OF THE LICENSED SOFTWARE OR DOCUMENTATION
+ * WILL NOT INFRINGE ANY THIRD PARTY PATENTS, COPYRIGHTS, TRADEMARKS OR OTHER
+ * RIGHTS.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package edu.ucla.sspace.common;
+
+import edu.ucla.sspace.common.Matrix;
+import edu.ucla.sspace.common.matrix.ArrayMatrix;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class MatricesTest {
+  @Test public void multiplyTest() {
+    double[] data1 = {1, 2, 3, 4, 1, 1, 1, 1, 5, 4, 3, 2};
+    Matrix leftMatrix = new ArrayMatrix(3, 4, data1);
+    double[] data2 = {1, 2, 1, 2, 1, 2, 1, 2};
+    Matrix rightMatrix = new ArrayMatrix(4, 2, data2);
+
+    Matrix result = Matrices.multiple(leftMatrix, rightMatrix);
+    double[][] expectations = {{10, 20},
+                               {4, 8},
+                               {14, 28}};
+    assertEquals(3, result.rows());
+    assertEquals(2, result.columns());
+    for (int r = 0; r < 3; ++r) {
+      for (int c = 0; c < 2; ++c) {
+        assertEquals(result.get(r,c), expectations[r][c], .00001);
+      }
+    }
+  }
+}
---
+++ trunk/sspace/test/edu/ucla/sspace/common/matrix/GrowingSparseMatrixTest.java
@@ -0,0 +1,52 @@
+package edu.ucla.sspace.common.matrix;
+
+import edu.ucla.sspace.common.Matrix;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GrowingSparseMatrixTest {
+  @Test public void testReplaceWithZero() {
+    double[][] testData = {{0, 0, 0, 1},
+                           {1, 0, 2, 0},
+                           {0, 0, 0, 0},
+                           {1, 2, 4, 5},
+                           {0, 0, 1, 0}};
+    GrowingSparseMatrix testMatrix = new GrowingSparseMatrix();
+    for (int i = 0; i < 5; ++i) {
+      for (int j = 0; j < 4; ++j) {
+        testMatrix.set(i, j, testData[i][j]);
+      }
+    }
+    testData[1][1] = 4;
+    testMatrix.set(1, 1, 4);
+    testData[1][0] = 0;
+    testMatrix.set(1, 0, 0);
+    for (int i = 0; i < 5; ++i) {
+      for (int j = 0; j < 4; ++j) {
+        assertEquals(testData[i][j], testMatrix.get(i, j), .0001);
+      }
+    }
+  }
+
+  @Test public void testMatrix() {
+    double[][] testData = {{0, 0, 0, 1},
+                           {1, 0, 2, 0},
+                           {0, 0, 0, 0},
+                           {1, 2, 4, 5},
+                           {0, 0, 1, 0}};
+    GrowingSparseMatrix testMatrix = new GrowingSparseMatrix();
+    for (int i = 0; i < 5; ++i) {
+      for (int j = 0; j < 4; ++j) {
+        testMatrix.set(i, j, testData[i][j]);
+      }
+    }
+    for (int i = 0; i < 5; ++i) {
+      for (int j = 0; j < 4; ++j) {
+        assertEquals(testData[i][j], testMatrix.get(i, j), .0001);
+      }
+    }
+  }
+}
