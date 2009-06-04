498d4f1e44ce3092d615e4307ccd26512d56a7ec
David.Jurgens <David.Jurgens@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-06-04 08:56:19 +0000

    BoundedMap updates
    M    sspace/src/edu/ucla/sspace/common/BoundedSortedMap.java
    
    - Added lots of missing javadoc
    
    - Added a reverse constructor that allows users to retain the lowest keys
    
    A    sspace/src/edu/ucla/sspace/common/BoundedSortedMultiMap.java
    
    - Added a MultiMap implementation
    
    - Allows users to retain either a set number of keys, or a set number of
      mappings
    
    - Also allows for a reverse, retain-lowest ordering

--- trunk/sspace/src/edu/ucla/sspace/common/BoundedSortedMap.java
+++ trunk/sspace/src/edu/ucla/sspace/common/BoundedSortedMap.java
@@ -21,34 +21,89 @@
 
 package edu.ucla.sspace.common;
 
+import java.util.Comparator;
 import java.util.Map;
 import java.util.TreeMap;
 
 /**
  * A {@code Map} implementation that grows to a fixed size and then retains only
- * the largest elements.
+ * a fixed number of the highest (largest) keys.  All keys used in this class
+ * must implements {@link Comarable}.
+ *
+ * @see BoundedSortedMultiMap
  */
 public class BoundedSortedMap<K,V> extends TreeMap<K,V> {
-  private final int bound;
 
-  private static final long serialVersionUID = 1;
+    private static final long serialVersionUID = 1;
 
-  public BoundedSortedMap(int bound) {
-    super();
-    this.bound = bound;
-  }
-
-  public V put(K key, V value) {
-    V old = super.put(key, value);
-    if (size() > bound) {
-      remove(firstKey());
+    /**
+     * The maximum number of mappings to retain.
+     */
+    private final int bound;
+    
+    /**
+     * Creatins an instance that will only retain the specified number of the
+     * largest (highest) keys.
+     *
+     * @param bound the number of mappings to retain
+     */
+    public BoundedSortedMap(int bound) {
+	this(bound, true);
     }
-    return old;
-  }
 
-  public void putAll(Map<? extends K,? extends V> m) {
-    for (Map.Entry<? extends K,? extends V> e : m.entrySet()) {
-      put(e.getKey(), e.getValue());
+    /**
+     * Creatins an instance that will only retain the specified number of keys,
+     * where the largest (highest) keys.
+     *
+     * @param bound the number of mappings to retain
+     * @param retainHighest {@true} if the highest elements are to be retained,
+     *        {@code false} if the lowest keys are to be retained
+     */
+    public BoundedSortedMap(int bound, boolean retainHighest) {
+	super(((retainHighest) ? null : new ReverseComparator<K>()));
+	this.bound = bound;
     }
-  }
+
+    /**
+     * Adds the key-value mapping to this map, and if the total number of
+     * mappings exceeds the bounds, removes either the currently lowest element,
+     * or if reversed, the currently highest element.
+     *
+     * @param key {@inheritDoc}
+     * @param value {@inheritDoc}
+     */
+    public V put(K key, V value) {
+	V old = super.put(key, value);
+	if (size() > bound) {
+	    remove(firstKey());
+	}
+	return old;
+    }
+
+    /**
+     * Adds all of the key-value mapping to this map, and if the total number of
+     * mappings exceeds the bounds, removes mappings until the size is within
+     * bounds.
+     *
+     * @param m {@inheritDoc}
+     */
+    public void putAll(Map<? extends K,? extends V> m) {
+	for (Map.Entry<? extends K,? extends V> e : m.entrySet()) {
+	    put(e.getKey(), e.getValue());
+	}
+    }
+
+    /**
+     * A comparator that results in the opposite ordering of the natural
+     * ordering from {@link Comparator#compareTo(Object,Object) compareTo}.
+     */
+    static final class ReverseComparator<K> implements Comparator<K> {
+	
+	// Assume that if the comparator is being used that the objects are
+	// instances of Comparable
+	@SuppressWarnings("unchecked")
+	public int compare(K c1, K c2) {
+	    return -(((Comparable)c1).compareTo(c2));
+	}
+    }
 }
