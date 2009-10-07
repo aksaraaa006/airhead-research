f7dc3a275ed167d8f57aeb6683899df9e1d36488
FozzietheBeat <FozzietheBeat@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-10-07 03:59:54 +0000

    add documentation to IndexBuilder

--- sspace/src/edu/ucla/sspace/common/IndexBuilder.java
+++ sspace/src/edu/ucla/sspace/common/IndexBuilder.java
@@ -31,34 +31,74 @@ import java.util.Queue;
  * An interface for classes which will maintain and utilize random indexes.
  * The main purpose of this of this class is to allow any algorithm which makes
  * use of some sort of random index, such as Random Indexing, Beagle, or other
- * varients, can easily swap out the type of random indexing used for
- * experimentation purposes.  Implementations should be thread safe.
+ * varients, can easily swap out the type of indexing used for
+ * experimentation purposes.
+ *
+ * The main work should be done in {@code updateMeaningWithTerm} where the words
+ * prior to the focus word are passed in as a {@code Queue}, and the words after
+ * the focus word are also passed in as a {@code Queue}.  Implementations are
+ * expected to traverse these {@code Queue}s in any order that makes sense, and
+ * should update {@code meaning} with whatever information is computed.
  */
 // TODO: Merge this with David's index building classes.  Make sure we can
 // incorporate or mix/match permutation abilities.
 public interface IndexBuilder {
 
-  /**
-   * Given a current meaning vector, update it using index vectors from a given
-   * window of words.
-   *
-   * @param meaning An existing meaning vector.  After calling this method, the
-   *        values of meaning will be updated according to some scheme.
-   * @param prevWords words appearing before the focus word whose meaning is
-   *        being updated
-   * @param nextWords words appearing after the focus word whose meaning is
-   *        being updated
-   */
-  public void updateMeaningWithTerm(Vector meaning,
-                                    Queue<String> prevWords,
-                                    Queue<String> nextWords);
+    /**
+     * Given a current meaning vector, update it using index vectors from a given
+     * window of words.
+     *
+     * @param meaning An existing meaning vector. After calling this method, the
+     *                values of meaning will be updated according to some scheme.
+     * @param prevWords words appearing before the focus word whose meaning is
+     *                being updated
+     * @param nextWords words appearing after the focus word whose meaning is
+     *                being updated
+     */
+    public void updateMeaningWithTerm(Vector meaning,
+                                      Queue<String> prevWords,
+                                      Queue<String> nextWords);
 
-  public void loadIndexVectors(File vectorFile);
+    /**
+     * Read in an set of existing index vectors from a binary file, along with any
+     * other data needed to represent the index vector accurately.
+     *
+     * @param vectorFile The file representing the stored {@code IndexBuiler}.
+     */
+    public void loadIndexVectors(File vectorFile);
 
-  public void saveIndexVectors(File vectorFile);
+    /**
+     * Store the set of existing index vectors to a binary file, along with any
+     * other data needed to represent the index vector accurately.
+     *
+     * @param vectorFile The file which will store the {@code IndexBuiler}.
+     */
+    public void saveIndexVectors(File vectorFile);
 
-  public Vector getSemanticVector();
+    /**
+     * Return a new {@code Vector} appropriate for use with a specific {@code
+     * IndexBuilder}.  For {@code IndexBuilder}s which use a dense representation
+     * internally, a {@code DenseVector} would be appropriate, whereas a {@code
+     * SparseVector} would be fitting for {@code IndexBuilder}s which utilize
+     * sparse representations internally.
+     *
+     * @return a {@Vector} having the same type as the vectors used internally.
+     */
+    public Vector getSemanticVector();
 
-  public int expectedSizeOfPrevWords();
-  public int expectedSizeOfNextWords();
+    /**
+     * Return the number of words expected before the focus word when examining
+     * a context window.
+     *
+     * @return The number of terms prior to the focus word.
+     */
+    public int expectedSizeOfPrevWords();
+
+    /**
+     * Return the number of words expected after the focus word when examining
+     * a context window.
+     *
+     * @return The number of terms after the focus word.
+     */
+    public int expectedSizeOfNextWords();
 }
