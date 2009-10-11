5ae4eb0a69073d652e1703b6d1f53f11069b6aaf
FozzietheBeat <FozzietheBeat@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-10-11 04:44:14 +0000

    Merging vectorized branch into the trunk and fixing an created compile time issues

--- sspace/src/edu/ucla/sspace/vector/DenseVector.java
+++ sspace/src/edu/ucla/sspace/vector/DenseVector.java
@@ -1,17 +1,41 @@
+/*
+ * Copyright 2009 Keith Stevens 
+ *
+ * This file is part of the S-Space package and is covered under the terms and
+ * conditions therein.
+ *
+ * The S-Space package is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation and distributed hereunder to you.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND NO REPRESENTATIONS OR WARRANTIES,
+ * EXPRESS OR IMPLIED ARE MADE.  BY WAY OF EXAMPLE, BUT NOT LIMITATION, WE MAKE
+ * NO REPRESENTATIONS OR WARRANTIES OF MERCHANT- ABILITY OR FITNESS FOR ANY
+ * PARTICULAR PURPOSE OR THAT THE USE OF THE LICENSED SOFTWARE OR DOCUMENTATION
+ * WILL NOT INFRINGE ANY THIRD PARTY PATENTS, COPYRIGHTS, TRADEMARKS OR OTHER
+ * RIGHTS.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package edu.ucla.sspace.vector;
+
+import java.util.Arrays;
+
 
 /**
  * A {@code Vector} where all values are held in memory. The underlying
  * implementation is simply an array of doubles.  <p>
  *
- * This class is thread-safe.
+ * @author Keith Stevens
 */
 public class DenseVector implements Vector {
 
     /**
      * The values of this {@code DenseVector}.
      */
-    private final double[] vector;
+    private double[] vector;
 
     /**
      * Create an {@code DenseVector} with all values starting at 0 with
@@ -34,44 +58,51 @@ public class DenseVector implements Vector {
     }
 	
     /**
-     * {@inheritDoc}
+     * Create a {@code DenseVector} by copying the values from another {@code
+     * Vector}.
+     *
+     * @param vector The {@code Vector} to copy from.
      */
-    public synchronized void addVector(Vector vector) {
-        // Skip vectors of different lengths.
-        if (vector.length() != length())
-            return;
-        for (int i = 0; i < length(); ++i) {
-            double value = vector.get(i);
-            add(i, value);
-        }
+    public DenseVector(Vector vector) {
+        this.vector = new double[vector.length()];
+
+        for (int i = 0; i < vector.length(); ++i)
+            set(i, vector.get(i));
     }
 
     /**
      * {@inheritDoc}
      */
-    public synchronized void add(int index, double delta) {
+    public double add(int index, double delta) {
         vector[index] += delta;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public synchronized void set(int index, double value) {
-        vector[index] = value;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public synchronized double get(int index) {
         return vector[index];
     }
 
     /**
      * {@inheritDoc}
      */
-    @SuppressWarnings("unchecked")
-    public synchronized double[] toArray(int size) {
+    public void set(int index, double value) {
+        vector[index] = value;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void set(double[] values) {
+        vector = Arrays.copyOf(values, vector.length);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public double get(int index) {
+        return vector[index];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public double[] toArray(int size) {
         double[] array = new double[size];
         for (int i = 0; i < size && i < vector.length; ++i)
             array[i] = vector[i];
@@ -81,7 +112,7 @@ public class DenseVector implements Vector {
     /**
      * {@inheritDoc}
      */
-    public double length() {
+    public int length() {
         return vector.length;
     }
 }
