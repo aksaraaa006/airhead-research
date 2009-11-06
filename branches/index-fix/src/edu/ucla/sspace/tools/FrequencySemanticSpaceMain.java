c6398d0413c0f64358290439b1a3fbc8562dae2f
FozzietheBeat <FozzietheBeat@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-11-06 19:45:46 +0000

    M    src/edu/ucla/sspace/tools/FrequencySemanticSpaceMain.java   - fix this to be better.
    M    src/edu/ucla/sspace/mains/GenericMain.java
      - fix spacing

--- branches/index-fix/src/edu/ucla/sspace/tools/FrequencySemanticSpaceMain.java
+++ branches/index-fix/src/edu/ucla/sspace/tools/FrequencySemanticSpaceMain.java
@@ -1,18 +1,27 @@
 package edu.ucla.sspace.tools;
 
+import edu.ucla.sspace.common.ArgOptions;
 import edu.ucla.sspace.common.SemanticSpace;
 
 import edu.ucla.sspace.mains.GenericMain;
 
+import edu.ucla.sspace.text.Document;
 import edu.ucla.sspace.text.IteratorFactory;
+
+import edu.ucla.sspace.util.TreeMultiMap;
 
 import edu.ucla.sspace.vector.DenseVector;
 import edu.ucla.sspace.vector.Vector;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.PrintWriter;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
@@ -24,7 +33,14 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class FrequencySemanticSpaceMain extends GenericMain {
 
+    private ConcurrentMap<String, AtomicInteger> termCounts;
+
     private FrequencySemanticSpaceMain() {
+        termCounts = new ConcurrentHashMap<String, AtomicInteger>();
+    }
+
+    public SemanticSpace getSpace() {
+        return null;
     }
 
     public void usage() {
@@ -32,11 +48,7 @@ public class FrequencySemanticSpaceMain extends GenericMain {
                            "<output-dir> " + argOptions.prettyPrint());
     }
 
-    public SemanticSpace getSpace() {
-        return new FrequencySemanticSpace();
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         FrequencySemanticSpaceMain main = new FrequencySemanticSpaceMain();
         try {
             main.run(args);
@@ -45,46 +57,80 @@ public class FrequencySemanticSpaceMain extends GenericMain {
         }
     }
 
-    private class FrequencySemanticSpace implements SemanticSpace {
+    public void run(String[] args) throws Exception {
+        if (args.length == 0) {
+            usage();
+            System.exit(1);
+        }
+        argOptions.parseOptions(args);
 
-        private ConcurrentMap<String, AtomicInteger> termCounts;
-
-        public FrequencySemanticSpace() {
-            termCounts = new ConcurrentHashMap<String, AtomicInteger>();
+        if (argOptions.numPositionalArgs() != 1) {
+            throw new IllegalArgumentException("must specify output directory");
         }
 
-        public void processDocument(BufferedReader document) {
-            Iterator<String> it = IteratorFactory.tokenize(document);
+        File outputFile = new File(argOptions.getPositionalArg(0));
 
-            while (it.hasNext()) {
-                String term = it.next().intern();
-                AtomicInteger oldCount = termCounts.putIfAbsent(term,
-                        new AtomicInteger(1));
-                if (oldCount != null)
-                    oldCount.incrementAndGet();
-            }
+        // all the documents are listed in one file, with one document per line
+        final Iterator<Document> docIter = getDocumentIterator();
+
+        // Check whether this class supports mutlithreading when deciding how
+        // many threads to use by default
+        int numThreads = Runtime.getRuntime().availableProcessors();
+        if (argOptions.hasOption("threads")) {
+            numThreads = argOptions.getIntOption("threads");
         }
 
-        public void processSpace(Properties props) {
+        Properties props = setupProperties();
+
+        // Initialize the IteratorFactory to tokenize the documents according to
+        // the specified configuration (e.g. filtering, compound words)
+        if (argOptions.hasOption("tokenFilter")) {
+            props.setProperty(IteratorFactory.TOKEN_FILTER_PROPERTY,
+                              argOptions.getStringOption("tokenFilter"));            
         }
 
-        public Set<String> getWords() {
-            return Collections.unmodifiableSet(termCounts.keySet());
+        if (argOptions.hasOption("compoundTokens")) {
+            props.setProperty(IteratorFactory.COMPOUND_TOKENS_FILE_PROPERTY,
+                              argOptions.getStringOption("compoundTokens"));
         }
+        IteratorFactory.setProperties(props);
 
-        public Vector getVector(String term) {
-            AtomicInteger count = termCounts.get(term);
-            Vector vector = new DenseVector(1);
-            vector.set(0, (count == null) ? 0 : count.get());
-            return vector;
+        Collection<Thread> threads = new LinkedList<Thread>();
+        for (int i = 0; i < numThreads; ++i) {
+            Thread t = new Thread() {
+                public void run() {
+                    while (docIter.hasNext()) {
+                        processDocument(docIter.next().reader());
+                    }
+                }
+            };
+            threads.add(t);
         }
+        for (Thread t : threads)
+            t.start();
+        for (Thread t : threads)
+            t.join();
 
-        public String getSpaceName() {
-            return "FrequencySemanticSpace";
-        }
+        TreeMultiMap<Integer, String> sortedFrequencyList =
+            new TreeMultiMap<Integer, String>();
+        for (Map.Entry<String, AtomicInteger> e : termCounts.entrySet())
+            sortedFrequencyList.put(e.getValue().get(), e.getKey());
 
-        public int getVectorLength() {
-            return 1;
+        PrintWriter outputWriter = new PrintWriter(outputFile);
+        for (Map.Entry<Integer, String> e : sortedFrequencyList.entrySet())
+            outputWriter.println(e.getValue() + "|" + e.getKey());
+        outputWriter.close();
+    }
+
+    public void processDocument(BufferedReader document) {
+        Iterator<String> it = IteratorFactory.tokenize(document);
+
+        while (it.hasNext()) {
+            String term = it.next().intern();
+            AtomicInteger oldCount = termCounts.putIfAbsent(
+                    term, new AtomicInteger(1));
+            if (oldCount != null)
+                oldCount.incrementAndGet();
         }
     }
 }
