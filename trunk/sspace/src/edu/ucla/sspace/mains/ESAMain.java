2eb4c37702254d484ab6ee60b40225066a662853
FozzietheBeat <FozzietheBeat@db9df836-f686-11dd-984d-ab748d24aa7e>
2009-11-06 02:57:27 +0000

    A    sspace/src/edu/ucla/sspace/common/DocumentVectorBuilder.java   - Add a document vector builder.  THis projects the document's contents into a     semantic space.
    M    sspace/src/edu/ucla/sspace/vector/Vectors.java
      - Return a sparse view vector when an immutable vector is requested.
    
    A    sspace/src/edu/ucla/sspace/vector/SparseViewVector.java
      - Add a sparse view vector so that views can get information about the
        sparsity of an immutable vector.
    
    M    sspace/src/edu/ucla/sspace/mains/ESAMain.java
      - Clean up the esa main to function as normal.  Currently has no options.
    
    M    sspace/src/edu/ucla/sspace/esa/ExplicitSemanticAnalysis.java
      - Remove document summation code from ESA and move it to the document vector
        builder.

--- trunk/sspace/src/edu/ucla/sspace/mains/ESAMain.java
+++ trunk/sspace/src/edu/ucla/sspace/mains/ESAMain.java
@@ -37,134 +37,47 @@ import java.util.Properties;
 
 /**
  * An executable class for running {@link ExplicitSemanticAnalysis} (ESA) from
- * the command line.  This class takes in several command line arguments.
- *
- * <ul>
- *
- * <li> {@code --overwrite=<boolean>} specifies whether to overwrite the
- *      existing output files.  The default is {@code true}.  If set to {@code
- *      false}, a unique integer is inserted into the file name.
- *
- * <li> {@code --verbose | -v} specifies whether to print runtime
- *      information to standard out
- *
- * </ul>
- *
- * <p>
- *
+ * the command line.  This class takes in the standard command line arguments
+ * found in {@code GenericMain}.
  *
  * @see ExplicitSemanticAnalysis
  *
  * @author David Jurgens
  */
 public class ESAMain extends GenericMain {
+
     private ESAMain() {
     }
-
-    /**
-     * Addes the {@code verbose} and {@code overwrite} options.
-     */
-    protected ArgOptions setupOptions() {
-	ArgOptions options = new ArgOptions();
-	options.addOption('w', "overwrite", "specifies whether to " +
-			  "overwrite the existing output", true, "BOOL",
-			  "Program Options");
-
-	options.addOption('v', "verbose", "prints verbose output",
-			  false, null, "Program Options");
-	addExtraOptions(options);
-	return options;
-    }
-
 
     /**
      * Adds all of the options to the {@link ArgOptions}.
      */
     protected void addExtraOptions(ArgOptions options) {
-
     }
 
     public static void main(String[] args) {
-	ESAMain esa = new ESAMain();
-	try {
-	    esa.run(args);
-	}
-	catch (Throwable t) {
-	    t.printStackTrace();
-	}
+        ESAMain esa = new ESAMain();
+        try {
+            esa.run(args);
+        }
+        catch (Throwable t) {
+            t.printStackTrace();
+        }
     }
 
     /**
      * {@inheritDoc} 
      */
     public SemanticSpace getSpace() {
-	return new ExplicitSemanticAnalysis();
-    }
-
-    public Properties setupProperties() {
-	// use the System properties in case the user specified them as
-	// -Dprop=<val> to the JVM directly.
-	Properties props = System.getProperties();
-	return props;
-    }
-
-    public void run(String[] args) throws IOException {
-	if (args.length == 0) {
-	    usage();
-	    System.exit(1);
-	}
-	argOptions.parseOptions(args);
-	
-	if (argOptions.numPositionalArgs() != 2) {
-	    usage();
-	    return;
-	}
-
-	File wikipediaSnapshotFile = new File(argOptions.getPositionalArg(0));
-
-	File outputDir = new File(argOptions.getPositionalArg(1));
-	if (!outputDir.isDirectory()){
-	    throw new IllegalArgumentException(
-		"output directory is not a directory: " + outputDir);
-	}
-
-	// process command line args
-	verbose = argOptions.hasOption('v') || argOptions.hasOption("verbose");
-
-	boolean overwrite = true;
-	if (argOptions.hasOption("overwrite")) {
-	    overwrite = argOptions.getBooleanOption("overwrite");
-	}
-
-	// process the Wikipedia snapshot
-	ExplicitSemanticAnalysis esa = new ExplicitSemanticAnalysis();
-	esa.processDocument(new BufferedReader(
-			        new FileReader(wikipediaSnapshotFile)));
-
-	// perform any remaining processing
-	esa.processSpace(setupProperties());
-
-	// determine where the space will go
-	File output = (overwrite)
-	    ? new File(outputDir, esa.getSpaceName()+ ".sspace")
-	    : File.createTempFile(esa.getSpaceName(), 
-				  ".sspace", outputDir);
-
-	// print the space
-	long startTime = System.currentTimeMillis();
-	SemanticSpaceIO.save(esa, output);
-	long endTime = System.currentTimeMillis();
-	verbose("printed space in %.3f seconds%n",
-		((endTime - startTime) / 1000d));
-
+        return new ExplicitSemanticAnalysis();
     }
 
     /**
      * Prints the instructions on how to execute this program to standard out.
      */
     public void usage() {
- 	System.out.println(
- 	    "usage: java ESAMain [options] <wikipedia-snapshot> <output-dir>\n"
-	    + argOptions.prettyPrint());
+         System.out.println(
+             "usage: java ESAMain [options] <output-dir>\n"
+            + argOptions.prettyPrint());
     }
 }
