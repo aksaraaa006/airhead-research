a97ea64e8f120eb06443b6aca9f833fe47058d2b
David.Jurgens <David.Jurgens@db9df836-f686-11dd-984d-ab748d24aa7e>
2010-01-07 20:04:40 +0000

    Updated ChildesParser to optionally generate augmented utterances based on the observer descriptions of each utterance.
    M    src/edu/ucla/sspace/text/IteratorFactory.java
    
    - Merged in changes from the trunk that support tokenizing Strings
    
    M    src/edu/ucla/sspace/tools/ChildesParser.java
    
    - Updated to generate new pseudo-utterances from the comment data

--- branches/hermit-fix/src/edu/ucla/sspace/tools/ChildesParser.java
+++ branches/hermit-fix/src/edu/ucla/sspace/tools/ChildesParser.java
@@ -23,10 +23,18 @@ package edu.ucla.sspace.tools;
 
 import edu.ucla.sspace.common.ArgOptions;
 
+import edu.ucla.sspace.text.IteratorFactory;
+
 import java.io.File;
 import java.io.IOError;
 import java.io.IOException;
 import java.io.PrintWriter;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -44,6 +52,7 @@ import org.w3c.dom.NodeList;
  * where a single xml file generates multiple documents.
  *
  * @author Keith Stevens
+ * @author David Jurgens
  */
 public class ChildesParser {
 
@@ -53,10 +62,17 @@ public class ChildesParser {
     private PrintWriter writer;
 
     /**
+     * {@code true} if the parser should generate augmented utterances from the
+     * comments when parsing
+     */
+    private final boolean generateAugmented;
+
+    /**
      * Creates the {@code ChildesParser}.   The given file name will be used to
      * write the extracted words.
      */
-    public ChildesParser(String outFile) {
+    public ChildesParser(String outFile, boolean generateAugmented) {
+        this.generateAugmented = generateAugmented;
         try {
             writer = new PrintWriter(outFile);
         } catch (IOException ioe) {
@@ -93,18 +109,89 @@ public class ChildesParser {
                 StringBuilder utteranceBuilder = new StringBuilder();
 
                 // Iterate over the words and get just the word text.
+                List<String> wordsInUtterance =
+                    new ArrayList<String>(words.getLength());
                 for (int j = 0; j < words.getLength(); ++j) {
-                    utteranceBuilder.append(
+                    wordsInUtterance.add(
                             words.item(j).getFirstChild().getNodeValue());
-                    utteranceBuilder.append(" ");
                 }
 
+                // Each of the <a> nodes contains additional information about
+                // the currnet utterances.  This may be syntactic information,
+                // comments on the scene, descriptions of the action, or
+                // clarification by the observer.  For all comments but the
+                // syntactic, use the comment text to create new pseudo
+                // utterances by combining tokens from the utterance with
+                // pseudo-tokens in the comment.  The pseudo-tokens have a
+                // "-GROUNDING" suffix which distiguishes them from tokens
+                // actually present in the uttered speech.
+                NodeList auxNodes = item.getElementsByTagName("a");
+                List<String> augmentedUtterances = new LinkedList<String>();
+                if (generateAugmented) {
+                    for (int j = 0; j < auxNodes.getLength(); ++j) {
+                        // Get any comment for the utterance
+                        Node n = auxNodes.item(j);
+                        String auxNodeType = n.getAttributes().
+                            getNamedItem("type").getNodeValue();
+
+                        // Get only those nodes that contain comments or
+                        // descriptions on the utterance that may be used to
+                        // ground the words being referred to.
+                        if (auxNodeType.equals("action")
+                            || auxNodeType.equals("actions")
+                            || auxNodeType.equals("addressee")
+                            || auxNodeType.equals("comments")
+                            || auxNodeType.equals("explanation")
+                            || auxNodeType.equals("gesture")
+                            || auxNodeType.equals("happening")
+                            || auxNodeType.equals("situation")) {
+                            
+                            String commentOnUtterance = 
+                                n.getFirstChild().getNodeValue();
+                            // Use the iterator factory to tokenize in the event
+                            // that the user has specified some form of token
+                            // filtering
+                            Iterator<String> tokenIter = 
+                                IteratorFactory.tokenize(commentOnUtterance);
+                            // For each of the tokens in the additional
+                            // information, create a pseudo-utterance using a
+                            // word from the actual utterance and an
+                            // grounding-token
+                            while (tokenIter.hasNext()) {
+                                String token = tokenIter.next();
+                                for (String word : wordsInUtterance) {
+                                    augmentedUtterances.add(word + " "
+                                        + token + "-GROUNDING");
+                                }
+                            }
+                        }
+                    }
+                }
+                    
                 // Write the utterance if an utterance is a document.
+                for (Iterator<String> it = wordsInUtterance.iterator(); 
+                         it.hasNext(); ) {
+                    utteranceBuilder.append(it.next());
+                    if (it.hasNext())
+                        utteranceBuilder.append(" ");
+                }
                 String utterance = utteranceBuilder.toString();
-                if (utterancePerDoc)
+                if (utterancePerDoc) {
                     print(utterance);
-                else  // otherwise save the utterance.
+                    // Print all the psuedo utterances constructed from the
+                    // comments
+                    for (String aug : augmentedUtterances)
+                        print(aug);
+                }
+                else {  // otherwise save the utterance.
                     fileBuilder.append(utterance).append(". ");
+                    // Print all the psuedo utterances constructed from the
+                    // comments.  Unlike the utterances, print these as separate
+                    // documents to avoid having them register as co-occurrences
+                    // with other utterances.
+                    for (String aug : augmentedUtterances)
+                        print(aug);
+                }
             }
 
             // Write all the utterances if the whole xml file is a document.
@@ -132,6 +219,14 @@ public class ChildesParser {
                           "otherwise all uterances in a file will be " +
                           "considered a document",
                           false, null, "Optional");
+        options.addOption('A', "augmentedUtterances",
+                          "Generates augmented utterances from comments " +
+                          "about the utterances", false, null, "Augmented");
+        options.addOption('F', "augmentedUtterancesFilter",
+                          "Specifes a token filter for which tokens in " +
+                          "comments are used to generate augmented utterances",
+                          true, "SPEC", "Augmented");
+
         options.addOption('d', "baseChildesDirectory",
                           "The base childes directory.  XML files will be " +
                           "searched for recursively from this base.  Use of " +
@@ -157,7 +252,17 @@ public class ChildesParser {
         boolean utterancePerDoc = false;
         utterancePerDoc = options.hasOption("utterancePerDoc");
 
-        ChildesParser parser = new ChildesParser(options.getPositionalArg(0));
+        boolean genAugmented = options.hasOption("augmentedUtterances");
+        if (genAugmented && options.hasOption("augmentedUtterancesFilter")) {
+            String filterConf = 
+                options.getStringOption("augmentedUtterancesFilter");
+            Properties p = System.getProperties();
+            p.setProperty(IteratorFactory.TOKEN_FILTER_PROPERTY, filterConf);
+            IteratorFactory.setProperties(p);
+        }
+
+        ChildesParser parser = 
+            new ChildesParser(options.getPositionalArg(0), genAugmented);
 
         // Process the given file list, if provided.
         if (options.hasOption("fileList")) {
