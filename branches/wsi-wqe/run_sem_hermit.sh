c17223625dc00ac200f8ebad24524e89208e0a56
FozzietheBeat <FozzietheBeat@db9df836-f686-11dd-984d-ab748d24aa7e>
2010-04-15 17:18:13 +0000

    M    wsi-wqe/run_sem_hermit.sh   - Update the run script

--- branches/wsi-wqe/run_sem_hermit.sh
+++ branches/wsi-wqe/run_sem_hermit.sh
@@ -2,16 +2,16 @@
 
 corpusDir=/fomor/corpora/SemEval2010
 outDir=$1
-CORPUS=$corpusDir/noun_training_file.txt,$corpusDir/verb_training_file.txt
+CORPUS=$corpusDir/semEvalTraining.txt,$corpusDir/noun_training_file.txt,$corpusDir/verb_training_file.txt,
 INDEX=""
-VALUES=""
+VALUES="yes"
 baseDir=/fomor/semEvalTraining/$1
 
-senseEvalUnSup=$corpusDir/key/scripts/unsup_eval.pl
+senseEvalUnSup=$corpusDir/trial-data/evaluation/unsupervised_evaluation/scripts/unsup_eval.pl
 semEvalUnSup="java -jar $corpusDir/unsupvmeasure.jar"
 
 function run() {
-  indexPrefix=$outDir/senseEval-hermit
+  indexPrefix=$baseDir/senseEval-hermit
   if [[ "$VALUES" -eq "" ]]
   then
     INDEX="-S $indexPrefix"
@@ -21,41 +21,56 @@ function run() {
     VALUES="yes"
   fi
 
-  systemKey=$outDir/senseval-$1p-$2h.key
-  testKey=$corpusDir/key/keys/senseinduction_test.key
-  nounKey=$corpusDir/key/keys/senseinduction_test_nouns.key
-  verbKey=$corpusDir/key/keys/senseinduction_test_verbs.key
-  trainKey=$corpusDir/key/keys/senseinduction_train.key
+  systemKey=$outDir/senseval-gap.key
+  testKey=$corpusDir/trial-data/evaluation/keys/allwords.key
+  #nounKey=$corpusDir/key/keys/senseinduction_test_nouns.key
+  #verbKey=$corpusDir/key/keys/senseinduction_test_verbs.key
+  #trainKey=$corpusDir/key/keys/senseinduction_train.key
 
   echo "Running FlyingHermit with $1 clusters and .$2 threshold."
   java -Xmx8g -server edu.ucla.sspace.mains.SenseEvalFlyingHermitMain \
-       -d $CORPUS -l 5000 -c $1 -h .$2 -s $3 $4 $INDEX \
-       -v $outDir/hermit.sspace 2> hermit.log 
+       -d $CORPUS -l 5000 -c $1 -h .$2 \
+       $4 $INDEX -s $3 \
+       -v $outDir/hermit-$1-$2${4}.sspace 2> hermit.log 
 
-  #echo "Running the senseval tester"
-  #java -Xmx8g -server edu.ucla.sspace.evaluation.SenseEvalTester \
-  #     -m $indexPrefix.index -p $indexPrefix.permutation \
-  #     -s $outDir/hermit.sspace -S $corpusDir/test/English_sense_induction.xml \
-  #     $systemKey -w $3
+  echo "Running the semEval tester"
+  java -Xmx8g -server edu.ucla.sspace.evaluation.SemEvalTester \
+       -m $indexPrefix.index -p $indexPrefix.permutation \
+       -s $outDir/hermit-$1-$2.sspace -w $3 \
+       $systemKey $corpusDir/test_data/nouns/*.xml \
+       $corpusDir/test_data/verbs/*.xml
+
+  if [[ "$4" -eq "" ]]
+  then
+    testPerm=""
+  else
+    testPerm="-p $indexPrefix.permutation"
+  fi
+
+  java -Xmx8g -server edu.ucla.sspace.evaluation.SemEvalTester \
+       -m $indexPrefix.index $testPerm \
+       -s $outDir/hermit-$1-$2${4}.sspace -w $3 \
+       $systemKey-$1-$2${4}.trial $corpusDir/trial-data/testing\ data/*.xml
 
   #echo "Evaluating FlyingHermit with SenseEval07"
-  #senseEvalName=$outDir/senseval-$1p-$2h
-  #evaluate
+  senseEvalName=$outDir/trial-senseval-$1-$2${4}
+  #$semEvalUnSup $systemKey.trial $testKey | tail -n 1 > $senseEvalName.vmeasure
+  evaluate $systemKey-$1-$2${4}.trial
 }
 
 function evaluate() {
-  $senseEvalUnSup -m fscore $systemKey $testKey > $senseEvalName.fscore
-  $senseEvalUnSup -m fscore -p n $systemKey $testKey > $senseEvalName.noun_fscore
-  $senseEvalUnSup -m fscore -p v $systemKey $testKey > $senseEvalName.verb_fscore
-  $senseEvalUnSup -m purity $systemKey $testKey > $senseEvalName.purity
-  $senseEvalUnSup -m entropy $systemKey $testKey > $senseEvalName.entropy
+  $senseEvalUnSup -m fscore $1 $testKey > $senseEvalName.fscore
+  $senseEvalUnSup -m fscore -p n $1 $testKey > $senseEvalName.noun_fscore
+  $senseEvalUnSup -m fscore -p v $1 $testKey > $senseEvalName.verb_fscore
+  $senseEvalUnSup -m purity $1 $testKey > $senseEvalName.purity
+  $senseEvalUnSup -m entropy $1 $testKey > $senseEvalName.entropy
 
-  pushd $corpusDir/key/scripts
-  ./sup_eval.sh $systemKey /tmp $trainKey $testKey > $senseEvalName.sup_recall
+  pushd $corpusDir/trial-data/evaluation/supervised_evaluation/scripts
+  ./sup_eval.sh $1 /tmp $trainKey > $senseEvalName.sup_recall
   popd
 
   echo "Evaluating FlyingHermit with SemEval10"
-  $semEvalUnSup $systemKey $testKey | tail -n 1 > $senseEvalName.vmeasure
+  $semEvalUnSup $1 $testKey | tail -n 1 > $senseEvalName.vmeasure
 }
 
 #run 15 45
@@ -68,20 +83,32 @@ then
 fi
 windowSize=$2
 #threshold=30
-for threshold in $(seq 05 5 95)
+for threshold in $(seq 05 10 95)
 do
   run 15 $threshold $windowSize "$3"
 done
 }
 
+#outDir=$baseDir
+#if [ ! -e $outDir ]
+#then
+#  mkdir -p $outDir
+#fi
+#run 15 15 1 ""
 # No permutation runs
-grid_search nop/max_window 100
-grid_search nop/small_window 1
-grid_search nop/medium_window 5
-grid_search nop/large_window 10 
+#grid_search perm/small_window 1 -P
+#grid_search perm/large_window 10 -P
+#grid_search perm/max_window 100 -P
+#grid_search nop/small_window 1
+#grid_search nop/large_window 10 
+#grid_search nop/max_window 100 
+
+outDir=$baseDir
+run 15 15 15 ""
+run 15 75 1 ""
 
 # Permutation runs
-grid_search perm/max_window 100 -P
-grid_search perm/small_window 1 -P
-grid_search perm/medium_window 5 -P
-grid_search perm/large_window 10 -P
+#grid_search nop/max_window 100
+#grid_search nop/medium_window 5
+#grid_search perm/max_window 100 -P
+#grid_search perm/medium_window 5 -P
