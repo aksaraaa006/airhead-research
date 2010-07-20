d97ea60042e76504e0a9874a5452b67ce5e4c1d9
FozzietheBeat <FozzietheBeat@db9df836-f686-11dd-984d-ab748d24aa7e>
2010-07-20 02:22:08 +0000

    A  + coals-fix A    coals-fix/test/edu/ucla/sspace/vector/MaskedSparseDoubleVectorViewTest.java A    coals-fix/test/edu/ucla/sspace/vector/MaskedDoubleVectorViewTest.java   - Add vector tests for the new masked views
    A    coals-fix/test/edu/ucla/sspace/matrix/CellMaskedMatrixTest.java
      - Add tests for the cell masked matrix.
    
    D    coals-fix/test/edu/ucla/sspace/dependency/DependencyExtractorTest.java
      - Removed, the corresponding class no longer exists
    
    M    coals-fix/src/edu/ucla/sspace/common/Similarity.java
      - Updated to fix the rank correlation
    
    M    coals-fix/src/edu/ucla/sspace/coals/Coals.java
    M    coals-fix/src/edu/ucla/sspace/vector/MaskedDoubleVectorView.java
    M    coals-fix/src/edu/ucla/sspace/matrix/CellMaskedMatrix.java
    M    coals-fix/src/edu/ucla/sspace/matrix/CellMaskedSparseMatrix.java
    M    coals-fix/build.xml

---
+++ branches/coals-fix/test/edu/ucla/sspace/vector/MaskedDoubleVectorViewTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2010 Keith Stevens 
+ *
+ * This file is part of the S-Space package and is covered under the terms and
+ * conditions therein.
+ *
+ * The S-Space package is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation and distributed hereunder to you.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND NO REPRESENTATIONS OR WARRANTIES,
+ * EXPRESS OR IMPLIED ARE MADE.  BY WAY OF EXAMPLE, BUT NOT LIMITATION, WE MAKE
+ * NO REPRESENTATIONS OR WARRANTIES OF MERCHANT- ABILITY OR FITNESS FOR ANY
+ * PARTICULAR PURPOSE OR THAT THE USE OF THE LICENSED SOFTWARE OR DOCUMENTATION
+ * WILL NOT INFRINGE ANY THIRD PARTY PATENTS, COPYRIGHTS, TRADEMARKS OR OTHER
+ * RIGHTS.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package edu.ucla.sspace.vector;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * Tests for the {@link MaskedDoubleVectorView} class.
+ */
+public class MaskedDoubleVectorViewTest {
+
+    @Test public void testScaledCreate() {
+        double[] values = new double[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        DoubleVector v = new DenseVector(values);
+        Map<Integer, Integer> mask = new HashMap<Integer, Integer>();
+        mask.put(0, 5);
+        mask.put(1, 9);
+        mask.put(2, 1);
+        DoubleVector masked = new MaskedDoubleVectorView(v, mask);
+
+        assertEquals(mask.size(), masked.length());
+        assertEquals(values[5], masked.get(0), .00001);
+        assertEquals(values[9], masked.get(1), .00001);
+        assertEquals(values[1], masked.get(2), .00001);
+    }
+}
